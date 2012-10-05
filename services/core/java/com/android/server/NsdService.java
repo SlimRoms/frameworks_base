fc14ece8596131aba4b2ca0fdf97f97f82aef943
Frode Isaksen <fisaksex>
2012-10-05 11:23:44 +0200

    wifi: grab MulticastLock when starting mDNS daemon
    Multicast DNS (mDNS) is using multicast (as the name implies), so
    grab the MulticastLock so that multicast packets are not dropped by
    the WiFi filters when screen off.
    
    Change-Id: I784bdec57f65d316c5aae960cf244a3cf80e9155
    Signed-off-by: Frode Isaksen <fisaksex@intel.com>
    Signed-off-by: Abdelmajid MLAYEH <abdelmajidx.mlayeh@intel.com>

--- services/core/java/com/android/server/NsdService.java
+++ services/core/java/com/android/server/NsdService.java
@@ -25,6 +25,7 @@ import android.net.nsd.NsdServiceInfo;
 import android.net.nsd.DnsSdTxtRecord;
 import android.net.nsd.INsdManager;
 import android.net.nsd.NsdManager;
+import android.net.wifi.WifiManager;
 import android.os.Binder;
 import android.os.Message;
 import android.os.Messenger;
@@ -81,6 +82,8 @@ public class NsdService extends INsdManager.Stub {
     private static final int BASE = Protocol.BASE_NSD_MANAGER;
     private static final int CMD_TO_STRING_COUNT = NsdManager.RESOLVE_SERVICE - BASE + 1;
     private static String[] sCmdToString = new String[CMD_TO_STRING_COUNT];
+
+    private WifiManager.MulticastLock mMulticastLock;
 
     static {
         sCmdToString[NsdManager.DISCOVER_SERVICES - BASE] = "DISCOVER";
@@ -685,6 +688,11 @@ public class NsdService extends INsdManager.Stub {
 
     private boolean startMDnsDaemon() {
         if (DBG) Slog.d(TAG, "startMDnsDaemon");
+        if (mMulticastLock == null) {
+            mMulticastLock = ((WifiManager) mContext.getSystemService(Context.WIFI_SERVICE)).
+                    createMulticastLock(TAG);
+            if (mMulticastLock != null) mMulticastLock.acquire();
+        }
         try {
             mNativeConnector.execute("mdnssd", "start-service");
         } catch(NativeDaemonConnectorException e) {
@@ -696,6 +704,10 @@ public class NsdService extends INsdManager.Stub {
 
     private boolean stopMDnsDaemon() {
         if (DBG) Slog.d(TAG, "stopMDnsDaemon");
+        if (mMulticastLock != null) {
+            mMulticastLock.release();
+            mMulticastLock = null;
+        }
         try {
             mNativeConnector.execute("mdnssd", "stop-service");
         } catch(NativeDaemonConnectorException e) {
