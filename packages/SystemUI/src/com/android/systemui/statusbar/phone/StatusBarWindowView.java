671ab51024ae1198f2c8ac96fb2ff13e236dbfed
krarvind <krarvind@gmail.com>
2012-10-16 16:17:34 -0300

    Single Finger Notification from 4.1.2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -53,8 +53,8 @@ public class StatusBarWindowView extends FrameLayout
         int maxHeight = getResources().getDimensionPixelSize(R.dimen.notification_row_max_height);
         mExpandHelper = new ExpandHelper(mContext, latestItems, minHeight, maxHeight);
         mExpandHelper.setEventSource(this);
+        mExpandHelper.setScrollView(scroller);
     }
-
     @Override
     public void dispatchWindowFocusChanged(boolean hasFocus) {
         this.setFocusableInTouchMode(hasFocus);
