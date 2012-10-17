11caf608d0af74ecbca215b1ee845e1a9b7c6081
krarvind <krarvind@gmail.com>
2012-10-17 00:20:14 -0300

    fix another error

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -495,6 +495,9 @@ class ServerThread extends Thread {
                 ServiceManager.addService(Context.PROFILE_SERVICE, profile);
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting Profile Manager", e);
+            }
+
+            try {
                 if (accountManager != null)
                     accountManager.systemReady();
             } catch (Throwable e) {
