277aabca4004101be603f88172723070823c0196
Christopher Tate <ctate@google.com>
2012-11-16 15:58:08 -0800

    Cache resource ID lookups in aapt
    Patchset 1 : Fixed tabs
    
    This speeds up certain workloads considerably, particularly
    those involved in buildling apps via the SDK.  Windows-based
    use should particularly benefit from the change.
    
    Change-Id: I29f4b3a77400b201ee219729cc28a5e359c0c5e8

--- tools/aapt/Android.mk
+++ tools/aapt/Android.mk
@@ -11,18 +11,19 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	AaptAssets.cpp \
-	Command.cpp \
-	CrunchCache.cpp \
-	FileFinder.cpp \
-	Main.cpp \
-	Package.cpp \
-	StringPool.cpp \
-	XMLNode.cpp \
-	ResourceFilter.cpp \
-	ResourceTable.cpp \
-	Images.cpp \
-	Resource.cpp \
+    AaptAssets.cpp \
+    Command.cpp \
+    CrunchCache.cpp \
+    FileFinder.cpp \
+    Main.cpp \
+    Package.cpp \
+    StringPool.cpp \
+    XMLNode.cpp \
+    ResourceFilter.cpp \
+    ResourceIdCache.cpp \
+    ResourceTable.cpp \
+    Images.cpp \
+    Resource.cpp \
     SourcePos.cpp \
     ZipEntry.cpp \
     ZipFile.cpp
@@ -40,12 +41,12 @@ LOCAL_C_INCLUDES += build/libs/host/include
 
 #LOCAL_WHOLE_STATIC_LIBRARIES := 
 LOCAL_STATIC_LIBRARIES := \
-	libhost \
-	libandroidfw \
-	libutils \
-	libcutils \
-	libexpat \
-	libpng
+    libhost \
+    libandroidfw \
+    libutils \
+    libcutils \
+    libexpat \
+    libpng
 
 ifeq ($(HOST_OS),linux)
 LOCAL_LDLIBS += -lrt -ldl -lpthread
