25c3c82e0ae55b5821596dd3d742f6292064ca3a
Arun Ravindran <arun.ravindran@intel.com>
2012-11-19 16:14:08 +0200

    Definition of new data profile based on apn type
    RIL_REQUEST_SETUP_DATA_CALL api has parameter profile id
    which is optional as mentioned in the api descriptions in
    ril.h. Though it is optional it is passed on always but
    the values passed are inadequate for api types for MMS,
    SUPL and HIPRI. These types will be used in RIL adaptation
    to select a specific channel (an HSI channel for example)
    for the SETUP request (a specific channel for MMS for example).
    Differentiating it based on apn name is not always good as
    there are operators (china mobile HK for example) providing
    same apn for default and mms.
    
    The value of the data profile is determined based on
    the the apn type define in Phone.java. This patch add the
    enumerations in framework base
    
    Change-Id: If76bce9e5845a33e08221ac2f2ddfddd6d22618b
    Signed-off-by: Alain Kouassu <alain.kouassu@intel.com>
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 32959

--- telephony/java/com/android/internal/telephony/RILConstants.java
+++ telephony/java/com/android/internal/telephony/RILConstants.java
@@ -188,6 +188,9 @@ cat include/telephony/ril.h | \
     public static final int DATA_PROFILE_IMS       = 2;
     public static final int DATA_PROFILE_FOTA      = 3;
     public static final int DATA_PROFILE_CBS       = 4;
+    public static final int DATA_PROFILE_MMS       = 5;
+    public static final int DATA_PROFILE_SUPL      = 6;
+    public static final int DATA_PROFILE_HIPRI     = 7;
     public static final int DATA_PROFILE_OEM_BASE  = 1000;
     public static final int DATA_PROFILE_INVALID   = 0xFFFFFFFF;
 
