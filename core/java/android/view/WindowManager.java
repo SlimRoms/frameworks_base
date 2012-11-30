9a608c92f8a1297610948b15313bb45967f97f69
Danesh M <daneshm90@gmail.com>
2012-11-29 22:26:21 -0500

    Camera : allow camera to use power key as shutter
    Provides a way for an app to take control of the power key.
    Used by the camera to make the power key control the shutter.
    
    Change-Id: I85a1e1761199f4604672be42a3a5005227f5451a
    (cherry picked from commit 15661444ae1faea831218f0c936b756de2f0698b)
    
    Prevent power key capture when screen is off
    
    The ability for an activity to capture the power key, which was
    added to support power key as shutter in the camera, should only
    allow the capture when the screen is on. Otherwise, if an activity
    that captures the power key is to the front when the device turns
    off, the user will be unable to turn it back on.
    
    Change-Id: Ib119d6914ec72554b404c1cc17eef3a932d5d402
    (cherry picked from commit 58897888df7e29f05688f8082129dc35d0efbac3)

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1439,6 +1439,12 @@ public interface WindowManager extends ViewManager {
          */
         public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
 
+    /**
+        * Window flag: Overrides default power key behavior
+         * @hide
+         */
+        public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
+
         /**
          * Control flags that are private to the platform.
          * @hide
