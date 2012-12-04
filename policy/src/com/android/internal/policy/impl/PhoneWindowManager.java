7a687107d723d5ad270d544126a38f192732a1eb
vivek mehta <mvivek@codeaurora.org>
2012-12-03 17:22:08 -0800

    Squashed commit of audio changes from CodeAurora
    MediaFile: Add DIVX and MPEG2TS file format
    
    Change-Id: I9f8cfbe8138c6d3d0d824bff2542b91d72a06b82
    
    audio: Define ANC headset and proxy device
    
    Change-Id: I681d73433d874ec66ec0b1f1f39869008d7d7d0d
    (cherry picked from commit 1e6c410eadfc510a9f116bb3da142214fe761225)
    
    audio: add support for QCOM WiFi display
    
    - Define QCOM WFD intents
    - Register and handle WFD intends in AudioSystem
    
    Change-Id: I1e5a73d4aa5595bed44d24652647c12b7ef4ae1f
    
    audio: add support for QCOM audio formats
    
    - Add support for EVRC, QCELP, and WMA formats
    
    Change-Id: If3676b88fa287b484ef8616c0a6b67177b61a720
    
    PhoneWindowManager: Changes for WFD.
    
    - WFD now sends WIFI_DISPLAY_VIDEO intent to set UI orientation.
    - PhoneWindowManager to listen for WFD intents.
    - Display on WFD should be same as seen on the HDMI, so set the
      orientation to HDMI orientation
    
    Change-Id: I638adaa906e2151b1c6ae496343714fe828f5dbd
    
    audio: add support for QCOM vocoders
    
    - Add support for EVRC,QCELP and other
      QCOM vocoders
    
    Change-Id: I9b2a18fe7d01a9198eb470e94e7176b7a6da2c7c

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -302,6 +302,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mSystemReady;
     boolean mSystemBooted;
     boolean mHdmiPlugged;
+    boolean mWifiDisplayConnected;
     int mDockMode = Intent.EXTRA_DOCK_STATE_UNDOCKED;
     int mLidOpenRotation;
     int mCarDockRotation;
@@ -1045,6 +1046,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         context.registerReceiver(mMultiuserReceiver, filter);
 
         mVibrator = (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+        // register for WIFI Display intents
+        IntentFilter wifiDisplayFilter = new IntentFilter(
+                                                Intent.ACTION_WIFI_DISPLAY_VIDEO);
+        Intent wifidisplayIntent = context.registerReceiver(
+                                      mWifiDisplayReceiver, wifiDisplayFilter);
+
         mLongPressVibePattern = getLongIntArray(mContext.getResources(),
                 com.android.internal.R.array.config_longPressVibePattern);
         mVirtualKeyVibePattern = getLongIntArray(mContext.getResources(),
@@ -3926,6 +3933,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
     };
+    BroadcastReceiver mWifiDisplayReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+            if (action.equals(Intent.ACTION_WIFI_DISPLAY_VIDEO)) {
+                int state = intent.getIntExtra("state", 0);
+                if(state == 1) {
+                    mWifiDisplayConnected = true;
+                } else {
+                    mWifiDisplayConnected = false;
+                }
+                updateRotation(true);
+            }
+        }
+    };
 
     BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
@@ -4141,9 +4162,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // enable 180 degree rotation while docked.
                 preferredRotation = mDeskDockEnablesAccelerometer
                         ? sensorRotation : mDeskDockRotation;
-            } else if (mHdmiPlugged && mHdmiRotationLock) {
+            } else if ((mHdmiPlugged || mWifiDisplayConnected) &&
+                                                        mHdmiRotationLock) {
                 // Ignore sensor when plugged into HDMI.
-                // Note that the dock orientation overrides the HDMI orientation.
+                // or Wifi display is connected
+                // Note that the dock orientation overrides the HDMI/Wifi
+                // orientation.
                 preferredRotation = mHdmiRotation;
             } else if ((mUserRotationMode == WindowManagerPolicy.USER_ROTATION_FREE
                             && (orientation == ActivityInfo.SCREEN_ORIENTATION_USER
