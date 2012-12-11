9b4407cd06f9c9e8fd7350c1e73f4d8932a62f5e
mnazim <mnm9994u@gmail.com>
2012-12-11 15:41:17 -0800

    Fix regression in ListView click handling
    Regression caused by patch a607223f34
    
    If an item click handler results in the ListView becoming detached
    from the window, the performClick runnable could be executed twice.
    Null out the mTouchModeReset runnable before running the click handler
    so that it is not run by the detach handler.
    
    Change-Id: I2fab60232027bb4876f9912088e0ffb9b17d308c

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -3416,6 +3416,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                             mTouchModeReset = new Runnable() {
                                 @Override
                                 public void run() {
+                                    mTouchModeReset = null;
                                     mTouchMode = TOUCH_MODE_REST;
                                     child.setPressed(false);
                                     setPressed(false);
