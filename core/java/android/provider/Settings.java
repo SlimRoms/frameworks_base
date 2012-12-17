b1a5353384dee7545824a9ac45baf6f0530e8485
kufikugel <kufikugel@googlemail.com>
2012-12-17 14:54:45 +0100

    Add var for dotted circle battery....
    next time please pay attention that the vars belong to that commit are not
    in another one ;)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2760,6 +2760,18 @@ public final class Settings {
          */
         public static final String STATUS_BAR_SIGNAL_TEXT = "status_bar_signal";
 
+        /**
+        * @hide
+        * Style of Battery
+        * 0 - Icon Only
+        * 1 - Text Only
+        * 2 - Icon Text
+        * 3 - Icon Centered Text
+        * 4 - Icon Circle
+        * 5 - Hide
+        */
+        public static final String STATUSBAR_BATTERY_ICON = "statusbar_battery_icon";
+
          /**
          * Whether to control brightness from status bar
          *
