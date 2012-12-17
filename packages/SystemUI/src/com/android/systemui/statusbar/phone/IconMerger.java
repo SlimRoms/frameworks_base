92125dc7ac0b39bb400d256943d7111a847b037b
Lars Greiss <kufikugel@googlemail.com>
2012-12-17 05:11:21 -0600

    Statusbar clock and date customizations
    Squashed:
    SystemUI: Status bar clock - fixes
    https://github.com/SlimRoms/frameworks_base/commit/a1e9a7a7222a7a2ecccd7288f5e26a198c9d1c89
    
    SystemUI: Ensure that the Clock is actually enabled in IconMerger
    https://github.com/SlimRoms/frameworks_base/commit/42393f259f08535b9fdab98462245830141a299a
    
    Some things changed on kitkat + our old code needed in general a bit
    rework. All done with this patchset.
    
    This commit add support for:
    
    - Clock and date enable/disable
    - Clock and date position
    - AM/PM styles multilang support
    - Color
    - Date format
    - Date style
    
    Change-Id: I2e725790f2605be6b38f6cfe0c04b70e1ad67e15

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/IconMerger.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/IconMerger.java
@@ -22,18 +22,21 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.Clock;
 
 public class IconMerger extends LinearLayout {
     private static final String TAG = "IconMerger";
     private static final boolean DEBUG = false;
 
-    private int mIconSize;
+    private int mIconWidth;
+    private int mClockAndDateWidth;
+    private boolean mCenterClock;
     private View mMoreView;
 
     public IconMerger(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        mIconSize = context.getResources().getDimensionPixelSize(
+        mIconWidth = context.getResources().getDimensionPixelSize(
                 R.dimen.status_bar_icon_size);
 
         if (DEBUG) {
@@ -50,7 +53,15 @@ public class IconMerger extends LinearLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         // we need to constrain this to an integral multiple of our children
         int width = getMeasuredWidth();
-        setMeasuredDimension(width - (width % mIconSize), getMeasuredHeight());
+
+        if (mCenterClock) {
+            final int totalWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+            final int usableWidth = (totalWidth - mClockAndDateWidth - 2 * mIconWidth) / 2;
+            if (width > usableWidth) {
+                width = usableWidth;
+            }
+        }
+        setMeasuredDimension(width - (width % mIconWidth), getMeasuredHeight());
     }
 
     @Override
@@ -69,8 +80,8 @@ public class IconMerger extends LinearLayout {
         }
         final boolean overflowShown = (mMoreView.getVisibility() == View.VISIBLE);
         // let's assume we have one more slot if the more icon is already showing
-        if (overflowShown) visibleChildren --;
-        final boolean moreRequired = visibleChildren * mIconSize > width;
+        if (!mCenterClock && overflowShown) visibleChildren --;
+        final boolean moreRequired = visibleChildren * mIconWidth > width;
         if (moreRequired != overflowShown) {
             post(new Runnable() {
                 @Override
@@ -79,5 +90,10 @@ public class IconMerger extends LinearLayout {
                 }
             });
         }
+    }
+
+    public void setClockAndDateStatus(int width, int mode, boolean enabled) {
+        mClockAndDateWidth = width;
+        mCenterClock = mode == Clock.STYLE_CLOCK_CENTER && enabled;
     }
 }
