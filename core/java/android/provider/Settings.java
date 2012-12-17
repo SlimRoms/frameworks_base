9d13ed3a0c44a2fef62f7755250d6f2f3efbfa8a
Lars Greiss <kufikugel@googlemail.com>
2012-12-17 05:11:21 -0600

    Statusbar clock and date customizations
    Squashed:
    SystemUI: Status bar clock - fixes
    https://github.com/SlimRoms/frameworks_base/commit/a1e9a7a7222a7a2ecccd7288f5e26a198c9d1c89
    
    SystemUI: Ensure that the Clock is actually enabled in IconMerger
    https://github.com/SlimRoms/frameworks_base/commit/42393f259f08535b9fdab98462245830141a299a
    
    Some things changed on kitkat + our old code needed in general a bit
    rework. All done with this patchset.
    
    This commit add support for:
    
    - Clock and date enable/disable
    - Clock and date position
    - AM/PM styles multilang support
    - Color
    - Date format
    - Date style
    
    Change-Id: I2e725790f2605be6b38f6cfe0c04b70e1ad67e15

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3250,6 +3250,62 @@ public final class Settings {
 
 
         /**
+         * Show or hide clock
+         * 0 - hide
+         * 1 - show (default)
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK = "status_bar_clock";
+
+        /**
+         * AM/PM Style for clock options
+         * 0 - Normal AM/PM
+         * 1 - Small AM/PM
+         * 2 - No AM/PM
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_AM_PM_STYLE = "statusbar_clock_am_pm_style";
+
+        /**
+         * Style of clock
+         * 0 - Hide Clock
+         * 1 - Right Clock
+         * 2 - Center Clock
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_STYLE = "statusbar_clock_style";
+
+        /**
+         * Setting for clock color
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
+
+        /**
+         * Shows custom date before clock time
+         * 0 - No Date
+         * 1 - Small Date
+         * 2 - Normal Date
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_DISPLAY = "statusbar_clock_date_display";
+
+        /**
+         * Sets the date string style
+         * 0 - Regular style
+         * 1 - Lowercase
+         * 2 - Uppercase
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_STYLE = "statusbar_clock_date_style";
+
+        /**
+         * Stores the java DateFormat string for the date
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
