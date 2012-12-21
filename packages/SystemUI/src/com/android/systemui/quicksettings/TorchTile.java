af0ac961377faff9222361cd1e6d94181be95490
DvTonder <david.vantonder@gmail.com>
2012-12-21 14:04:08 -0500

    Quick Settings: Cleanup Torch tile (2 of 2)
    Change-Id: Iacd9e58a4e45ed2526bde406c23647e6da9b76ec

--- packages/SystemUI/src/com/android/systemui/quicksettings/TorchTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/TorchTile.java
@@ -16,12 +16,9 @@
 
 package com.android.systemui.quicksettings;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Handler;
 import android.provider.Settings;
@@ -29,16 +26,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import com.android.systemui.R;
-import com.android.systemui.statusbar.phone.QuickSettingsController;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
 
-public class FlashLightTile extends QuickSettingsTile {
+public class TorchTile extends QuickSettingsTile {
 
-    public FlashLightTile(Context context, LayoutInflater inflater,
+    public TorchTile(Context context, LayoutInflater inflater,
             QuickSettingsContainerView container,
             QuickSettingsController qsc, Handler handler) {
         super(context, inflater, container, qsc);
+
         updateTileState();
+
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +45,7 @@ public class FlashLightTile extends QuickSettingsTile {
                 mContext.sendBroadcast(i);
             }
         };
+
         mOnLongClick = new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -55,18 +55,22 @@ public class FlashLightTile extends QuickSettingsTile {
                 return true;
             }
         };
+
         qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.TORCH_STATE), this);
     }
 
     private void updateTileState() {
         boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.TORCH_STATE, 0) == 1;
+        String label = mContext.getString(R.string.quick_settings_torch);
+
+        // Show OFF next to the label when in OFF state, ON/IN USE is indicated by the color
         if(enabled) {
-            mDrawable = R.drawable.ic_qs_flashlight_on;
-            mLabel = mContext.getString(R.string.quick_settings_label_enabled);
+            mDrawable = R.drawable.ic_qs_torch_on;
+            mLabel = label;
         } else {
-            mDrawable = R.drawable.ic_qs_flashlight_off;
-            mLabel = mContext.getString(R.string.quick_settings_label_disabled);
+            mDrawable = R.drawable.ic_qs_torch_off;
+            mLabel = label + " " + mContext.getString(R.string.quick_settings_label_disabled);
         }
     }
 
