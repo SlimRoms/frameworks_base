90945fa183f860206ecb6dfc2fa0e46860e82433
mnazim <mnm9994u@gmail.com>
2012-12-23 14:28:04 -0500

    Do not show camera widget by default
    Change-Id: I157e021fb7a70d76b276c7e17a098f9a16dfc494

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -1051,7 +1051,7 @@ public class KeyguardHostView extends KeyguardViewBase {
         // inflate system-provided camera?
         if (!mSafeModeEnabled && !cameraDisabledByDpm()
                 && Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.KG_CAMERA_WIDGET, 1) == 1) {
+                Settings.System.KG_CAMERA_WIDGET, 0) == 1) {
             View cameraWidget =
                     CameraWidgetFrame.create(mContext, mCameraWidgetCallbacks, mActivityLauncher);
             if (cameraWidget != null) {
