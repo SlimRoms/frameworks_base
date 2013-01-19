837e867f36535b649d049a727696ebd23b238321
Lars Greiss <kufikugel@googlemail.com>
2013-01-20 00:47:06 +0100

    Fix Notification drawer color and alpha
    did not stick after reboot.
    
    fixed
    
    Change-Id: Iab5197f8b9189ba331a09181a6eeba29a8f198ec

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -703,6 +703,9 @@ public class PhoneStatusBar extends BaseStatusBar {
             });
         }
 
+        // Set notification background
+        setNotificationWallpaperHelper();
+
         // Quick Settings (where available, some restrictions apply)
         if (mHasSettingsPanel) {
             // first, figure out where quick settings should be inflated
