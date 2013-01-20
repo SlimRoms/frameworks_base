4002fca8c0f2f4a8eeeefb8b363b2671d05e9308
tchaari <tarak.chaari@gmail.com>
2013-01-20 17:06:54 +0100

    Reworked Wifi AP Button in powerwidget to be like quick settings
    This fixes issue 93 in redmine: WiFi is brought back to its previous
    state after disabling WiFi AP.
    
    Change-Id: I22047e17092b8706512ec52128b0405b1a4bf8a7

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/WifiApButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/WifiApButton.java
@@ -21,127 +21,54 @@ import com.android.systemui.R;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.net.wifi.WifiManager;
-import android.os.AsyncTask;
-import android.util.Log;
+import android.content.ContentResolver;
+import android.provider.Settings;
 
 public class WifiApButton extends PowerButton {
 
-    private static final StateTracker sWifiApState = new WifiApStateTracker();
-
-    /**
-     * Subclass of StateTracker to get/set Wifi AP state.
-     */
-    private static final class WifiApStateTracker extends StateTracker {
-        @Override
-        public int getActualState(Context context) {
-            WifiManager wifiManager = (WifiManager) context
-            .getSystemService(Context.WIFI_SERVICE);
-            if (wifiManager != null) {
-                return wifiApStateToFiveState(wifiManager.getWifiApState());
-            }
-            return STATE_UNKNOWN;
-        }
-
-        @Override
-        protected void requestStateChange(Context context,
-                final boolean desiredState) {
-
-            final WifiManager wifiManager = (WifiManager) context
-            .getSystemService(Context.WIFI_SERVICE);
-            if (wifiManager == null) {
-                Log.d("WifiAPManager", "No wifiManager.");
-                return;
-            }
-            Log.i("WifiAp", "Setting: " + desiredState);
-
-            // Actually request the Wi-Fi AP change and persistent
-            // settings write off the UI thread, as it can take a
-            // user-noticeable amount of time, especially if there's
-            // disk contention.
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... args) {
-                    /**
-                     * Disable Wif if enabling tethering
-                     */
-                    int wifiState = wifiManager.getWifiState();
-                    if (desiredState
-                            && ((wifiState == WifiManager.WIFI_STATE_ENABLING) || (wifiState == WifiManager.WIFI_STATE_ENABLED))) {
-                        wifiManager.setWifiEnabled(false);
-                    }
-
-                    wifiManager.setWifiApEnabled(null, desiredState);
-                    Log.i("WifiAp", "Async Setting: " + desiredState);
-                    return null;
-                }
-            }.execute();
-        }
-
-        @Override
-        public void onActualStateChange(Context context, Intent intent) {
-
-            if (!WifiManager.WIFI_AP_STATE_CHANGED_ACTION.equals(intent
-                    .getAction())) {
-                return;
-            }
-            int wifiState = intent
-                .getIntExtra(WifiManager.EXTRA_WIFI_AP_STATE, -1);
-            int widgetState=wifiApStateToFiveState(wifiState);
-            setCurrentState(context, widgetState);
-        }
-
-        /**
-         * Converts WifiManager's state values into our Wifi/WifiAP/Bluetooth-common
-         * state values.
-         */
-        private static int wifiApStateToFiveState(int wifiState) {
-            switch (wifiState) {
-                case WifiManager.WIFI_AP_STATE_DISABLED:
-                    return STATE_DISABLED;
-                case WifiManager.WIFI_AP_STATE_ENABLED:
-                    return STATE_ENABLED;
-                case WifiManager.WIFI_AP_STATE_DISABLING:
-                    return STATE_TURNING_OFF;
-                case WifiManager.WIFI_AP_STATE_ENABLING:
-                    return STATE_TURNING_ON;
-                default:
-                    return STATE_UNKNOWN;
-            }
-        }
-    }
+    private static WifiManager mWifiManager;
 
     public WifiApButton() { mType = BUTTON_WIFIAP; }
 
     @Override
+    public void afterInit(){
+        if (mWifiManager == null) mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+    }
+
+    @Override
     protected void updateState(Context context) {
-        mState = sWifiApState.getTriState(context);
-        switch (mState) {
-            case STATE_DISABLED:
-                mIcon = R.drawable.stat_wifi_ap_off;
-                break;
-            case STATE_ENABLED:
+        afterInit();
+        int state = mWifiManager.getWifiApState();
+        switch (state) {
+            case WifiManager.WIFI_AP_STATE_ENABLING:
+            case WifiManager.WIFI_AP_STATE_ENABLED:
                 mIcon = R.drawable.stat_wifi_ap_on;
                 break;
-            case STATE_INTERMEDIATE:
-                // In the transitional state, the bottom green bar
-                // shows the tri-state (on, off, transitioning), but
-                // the top dark-gray-or-bright-white logo shows the
-                // user's intent. This is much easier to see in
-                // sunlight.
-                if (sWifiApState.isTurningOn()) {
-                    mIcon = R.drawable.stat_wifi_ap_on;
-                } else {
-                    mIcon = R.drawable.stat_wifi_ap_off;
-                }
+            case WifiManager.WIFI_AP_STATE_DISABLING:
+            case WifiManager.WIFI_AP_STATE_DISABLED:
+                mIcon = R.drawable.stat_wifi_ap_off;
+                break;
+            default:
+                mIcon = R.drawable.stat_wifi_ap_off;
                 break;
         }
     }
 
     @Override
     protected void toggleState(Context context) {
-        sWifiApState.toggleState(context);
+        afterInit();
+        int state = mWifiManager.getWifiApState();
+        switch (state) {
+            case WifiManager.WIFI_AP_STATE_ENABLING:
+            case WifiManager.WIFI_AP_STATE_ENABLED:
+                setSoftapEnabled(false);
+                break;
+            case WifiManager.WIFI_AP_STATE_DISABLING:
+            case WifiManager.WIFI_AP_STATE_DISABLED:
+                setSoftapEnabled(true);
+                break;
+        }
     }
 
     @Override
@@ -155,9 +82,39 @@ public class WifiApButton extends PowerButton {
         return true;
     }
 
+    private void setSoftapEnabled(boolean enable) {
+        final ContentResolver cr = mContext.getContentResolver();
+        /**
+         * Disable Wifi if enabling tethering
+         */
+        int wifiState = mWifiManager.getWifiState();
+        if (enable && ((wifiState == WifiManager.WIFI_STATE_ENABLING) ||
+                    (wifiState == WifiManager.WIFI_STATE_ENABLED))) {
+            mWifiManager.setWifiEnabled(false);
+            Settings.Global.putInt(cr, Settings.Global.WIFI_SAVED_STATE, 1);
+        }
+        // Turn on the Wifi AP
+        mWifiManager.setWifiApEnabled(null, enable);
+        /**
+         *  If needed, restore Wifi on tether disable
+         */
+        if (!enable) {
+            int wifiSavedState = 0;
+            try {
+                wifiSavedState = Settings.Global.getInt(cr, Settings.Global.WIFI_SAVED_STATE);
+            } catch (Settings.SettingNotFoundException e) {
+                // Do nothing here
+            }
+            if (wifiSavedState == 1) {
+                mWifiManager.setWifiEnabled(true);
+                Settings.Global.putInt(cr, Settings.Global.WIFI_SAVED_STATE, 0);
+            }
+        }
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        sWifiApState.onActualStateChange(context, intent);
+        update(context);
     }
 
     @Override
