6f8270b5a851d21f7f7bd6d4e00ab60875a16bbd
Lars Greiss <kufikugel@googlemail.com>
2013-01-23 03:28:51 +0100

    Frameworks: Fix first animation to QS
    on click of qs settings button
    
    Change-Id: I7fbc4bc162aabeedb6b88c3046403c12ebb2e6bf

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1693,6 +1693,11 @@ public class PhoneStatusBar extends BaseStatusBar {
         final boolean halfWayDone = mScrollView.getVisibility() == View.VISIBLE;
         final int zeroOutDelays = halfWayDone ? 0 : 1;
 
+        if (!halfWayDone) {
+            mScrollView.setScaleX(0f);
+            mFlipSettingsView.setScaleX(1f);
+        }
+
         mScrollView.setVisibility(View.VISIBLE);
         mScrollViewAnim = start(
             startDelay(FLIP_DURATION_OUT * zeroOutDelays,
@@ -1840,6 +1845,11 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         final boolean halfWayDone = mFlipSettingsView.getVisibility() == View.VISIBLE;
         final int zeroOutDelays = halfWayDone ? 0 : 1;
+
+        if (!halfWayDone) {
+            mFlipSettingsView.setScaleX(0f);
+            mScrollView.setScaleX(1f);
+        }
 
         mFlipSettingsView.setVisibility(View.VISIBLE);
         mFlipSettingsViewAnim = start(
