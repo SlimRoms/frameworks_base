1284dddee7d0eb3dc7592ef960afa738f17b97f2
Lars Greiss <kufikugel@googlemail.com>
2013-01-24 19:07:43 +0100

    Fix Ram Bar calculating
    seem realy my fever last days made me to a bad programmer lol
    fix for wrong calculated other value
    fixes also sometimes weird negative views
    
    cheers
    
    Change-Id: I14a4f2230f5dddc5104e5fa0cef474ec00676847

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -100,7 +100,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     private ImageView mClearRecents;
     private LinearColorBar mRamUsageBar;
 
-    private long mTotalUsedMemory;
+    private long mFreeMemory;
     private long mTotalMemory;
     private long mCachedMemory;
     private long mActiveMemory;
@@ -834,8 +834,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                         freeMem = mTotalMemory - mActiveMemory - mCachedMemory;
                         break;
                     case 3:
-                        usedMem = mActiveMemory + mCachedMemory + mTotalUsedMemory;
-                        freeMem = mTotalMemory - mActiveMemory - mCachedMemory - mTotalUsedMemory;
+                        usedMem = mTotalMemory - mFreeMemory;
+                        freeMem = mFreeMemory;
                         break;
                 }
 
@@ -849,7 +849,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 mRamText.setText(getResources().getString(
                         R.string.memory));
                 float totalMem = mTotalMemory;
-                float totalShownMem = mTotalUsedMemory / totalMem;
+                float totalShownMem = (mTotalMemory - mFreeMemory - mCachedMemory -  mActiveMemory)/ totalMem;
                 float totalActiveMem = mActiveMemory / totalMem;
                 float totalCachedMem = mCachedMemory / totalMem;
                 mRamUsageBar.setRatios(totalShownMem, totalCachedMem, totalActiveMem);
@@ -920,7 +920,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 }
             }
         } catch (IOException e) {}
-        mTotalUsedMemory = result;
+        mFreeMemory = result;
 
         try {
             String firstLine = readLine("/proc/meminfo", 6);
