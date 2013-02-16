f6d8d47cabcbf509841734b1534e1e72888ec0a2
Laszlo David <laszlo.david@gmail.com>
2013-02-16 01:23:12 +0100

    Forward Port: Swap volume buttons when the screen is rotated by 90 or 180 degrees
    Change to default behaviour not configurable due that it is now
    more natural
    
    Change-Id: I323b647066e8c73fac60067141d1add7b2f9962d

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -45,6 +45,8 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Surface;
+import android.view.WindowManager;
 
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -68,6 +70,8 @@ public class AudioManager {
     private static String TAG = "AudioManager";
     AudioPortEventHandler mAudioPortEventHandler;
     private static ArrayList<MediaPlayerInfo> mMediaPlayers;
+    private final WindowManager mWindowManager;
+
     /**
      * Broadcast intent, a hint for applications that audio is about to become
      * 'noisy' due to a change in audio outputs. For example, this intent may
@@ -619,8 +623,9 @@ public class AudioManager {
         mAudioPortEventHandler = new AudioPortEventHandler(this);
         mUseFixedVolume = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useFixedVolume);
-        mMediaPlayers = new ArrayList<MediaPlayerInfo>(1);
 
+        mMediaPlayers = new ArrayList<MediaPlayerInfo>(1);
+        mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
     }
 
     private static IAudioService getService()
@@ -699,21 +704,24 @@ public class AudioManager {
                  * Adjust the volume in on key down since it is more
                  * responsive to the user.
                  */
+                int direction;
+                int rotation = mWindowManager.getDefaultDisplay().getRotation();
+                if (rotation == Surface.ROTATION_90
+                        || rotation == Surface.ROTATION_180) {
+                    direction = keyCode == KeyEvent.KEYCODE_VOLUME_UP
+                            ? ADJUST_LOWER
+                            : ADJUST_RAISE;
+                } else {
+                    direction = keyCode == KeyEvent.KEYCODE_VOLUME_UP
+                            ? ADJUST_RAISE
+                            : ADJUST_LOWER;
+                }
                 int flags = FLAG_SHOW_UI | FLAG_VIBRATE;
 
                 if (mUseMasterVolume) {
-                    adjustMasterVolume(
-                            keyCode == KeyEvent.KEYCODE_VOLUME_UP
-                                    ? ADJUST_RAISE
-                                    : ADJUST_LOWER,
-                            flags);
+                    adjustMasterVolume(direction, flags);
                 } else {
-                    adjustSuggestedStreamVolume(
-                            keyCode == KeyEvent.KEYCODE_VOLUME_UP
-                                    ? ADJUST_RAISE
-                                    : ADJUST_LOWER,
-                            stream,
-                            flags);
+                    adjustSuggestedStreamVolume(direction, stream, flags);
                 }
                 break;
             case KeyEvent.KEYCODE_VOLUME_MUTE:
