444a102001ddf67fa19e48d9830859c84e81e126
tchaari <tarak.chaari@gmail.com>
2013-04-07 12:48:12 +0100

    Remove Favorite contact tile from quick settings (1/2)
    It is now covered by Custom shortcut tile
    
    Change-Id: I28c6eab5dce4024e1f3987a0cfd73dc6fb08a660

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -68,7 +68,6 @@ public class QuickSettingsController {
     public static final String TILE_BRIGHTNESS = "toggleBrightness";
     public static final String TILE_BUGREPORT = "toggleBugreport";
     public static final String TILE_EXPANDED_DESKTOP = "toggleExpandedDesktop";
-    public static final String TILE_FAVCONTACT = "toggleFavoriteContact";
     public static final String TILE_FCHARGE = "toggleFCharge";
     public static final String TILE_GPS = "toggleGPS";
     public static final String TILE_IME = "toggleIME";
@@ -107,7 +106,6 @@ public class QuickSettingsController {
         TILES_CLASSES.put(TILE_BRIGHTNESS, "com.android.systemui.quicksettings.BrightnessTile");
         TILES_CLASSES.put(TILE_BUGREPORT, "com.android.systemui.quicksettings.BugReportTile");
         TILES_CLASSES.put(TILE_EXPANDED_DESKTOP, "com.android.systemui.quicksettings.ExpandedDesktopTile");
-        TILES_CLASSES.put(TILE_FAVCONTACT, "com.android.systemui.quicksettings.FavoriteContactTile");
         TILES_CLASSES.put(TILE_FCHARGE, "com.android.systemui.quicksettings.FChargeTile");
         TILES_CLASSES.put(TILE_GPS, "com.android.systemui.quicksettings.GPSTile");
         TILES_CLASSES.put(TILE_IME, "com.android.systemui.quicksettings.InputMethodTile");
