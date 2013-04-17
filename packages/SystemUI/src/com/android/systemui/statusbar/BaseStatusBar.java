a0662c5e4f6b97da8279b1b8ec9f40456adda773
Lars Greiss <kufikugel@googlemail.com>
2013-04-17 13:44:47 +0200

    Frameworks: fix possible NPE
    due a race condition this can happen....prevend it
    
    cheers
    
    Change-Id: I65222389c6b0beecaebed915644ab5e800c5f119

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1477,7 +1477,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                 lp.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_BEHIND;
 
                 mWindowManager.addView(mPieContainer, lp);
-                mPieController.attachTo(mPieContainer);
+                if (mPieController != null) {
+                    mPieController.attachTo(mPieContainer);
+                }
             }
 
             // add or update pie triggers
