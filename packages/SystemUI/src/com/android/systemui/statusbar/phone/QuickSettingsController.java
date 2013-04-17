e78dc279977f978bce3b0ee6179dd935c51cb407
Lars Greiss <kufikugel@googlemail.com>
2013-04-17 15:31:23 +0200

    Frameworks: Fix possible NPE
    if a QS tile does not exist any more...in this case we
    had favorite contacts and removed that feature....ppl which
    setuped it before on older builds and do a dirty flash
    will run into a FC then
    
    cheers
    
    Change-Id: Ia05a7cfc91c0dc43e15df8f30c355f414c95207f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -192,6 +192,9 @@ public class QuickSettingsController {
         QuickSettingsTile qs = null;
         if (condition){
            try{
+               if (TILES_CLASSES.get(tile) == null) {
+                   return qs;
+               }
                DexClassLoader classLoader = new DexClassLoader(
                        "/system/app/SystemUI.apk", mContext.getFilesDir().getAbsolutePath(),
                        null, getClass().getClassLoader());
