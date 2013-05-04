68420637bb8f2bac7de00ec586ffbccfd121140c
Steve Kondik <shade@chemlab.org>
2013-05-04 11:56:08 -0700

    power: Add panel auto-brightness configuration
     * Some panel hardware requires that we twiddle some knobs in sysfs to
       enable proper display calibration (gamma, etc) when auto-brightness
       is enabled. Add a configuration value to handle this.
    
    Change-Id: I3c793c3520fc6f9a18d5a3abf1fe0df803c5e580

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -184,6 +184,7 @@ public final class PowerManagerService extends IPowerManager.Stub
     private WirelessChargerDetector mWirelessChargerDetector;
     private SettingsObserver mSettingsObserver;
     private DreamManagerService mDreamManager;
+    private AutoBrightnessHandler mAutoBrightnessHandler;
     private LightsService.Light mAttentionLight;
     private LightsService.Light mButtonsLight;
     private LightsService.Light mKeyboardLight;
@@ -345,6 +346,10 @@ public final class PowerManagerService extends IPowerManager.Stub
     // Use 0 if there is no adjustment.
     private float mScreenAutoBrightnessAdjustmentSetting;
 
+    // Alternative auto panel value for special devices
+    // default = -1, set over config.xml
+    private int mPanelAutoValue = -1;
+
     // The screen brightness mode.
     // One of the Settings.System.SCREEN_BRIGHTNESS_MODE_* constants.
     private int mScreenBrightnessModeSetting;
@@ -431,6 +436,11 @@ public final class PowerManagerService extends IPowerManager.Stub
         // activity manager is not running when the constructor is called, so we
         // have to defer setting the screen state until this point.
         mDisplayBlanker.unblankAllDisplays();
+
+        mAutoBrightnessHandler = new AutoBrightnessHandler(context);
+        mPanelAutoValue = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_panelAutoBrightnessValue);
+
     }
 
     public void setPolicy(WindowManagerPolicy policy) {
@@ -596,9 +606,16 @@ public final class PowerManagerService extends IPowerManager.Stub
             mTemporaryScreenAutoBrightnessAdjustmentSettingOverride = Float.NaN;
         }
 
+        final int oldScreenBrightnessModeSetting =
+                mScreenBrightnessModeSetting;
         mScreenBrightnessModeSetting = Settings.System.getIntForUser(resolver,
                 Settings.System.SCREEN_BRIGHTNESS_MODE,
                 Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL, UserHandle.USER_CURRENT);
+        // Set autobrightness/brigthness mode change for devices different then default
+        if (oldScreenBrightnessModeSetting != mScreenBrightnessModeSetting
+                && mPanelAutoValue > -1) {
+            mAutoBrightnessHandler.onAutoBrightnessChanged(mScreenBrightnessModeSetting, mPanelAutoValue);
+        }
 
         mDirty |= DIRTY_SETTINGS;
     }
