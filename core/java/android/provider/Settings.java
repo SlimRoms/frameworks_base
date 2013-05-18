ad2980a4564bd983ab785cfb1d3d16627d642c88
blk_jack <jcorcoran+github@gmail.com>
2013-05-18 05:03:42 -0400

    Framework: Option to force keyboard to show enter key (1/2)
    Original SlimRoms feature!  Not everyone likes emoticons :-(
    
    Change-Id: Idc093d8fa14633ccf2817fadcfb09d32768445b0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3605,6 +3605,12 @@ public final class Settings {
         public static final String KEYBOARD_ROTATION_TIMEOUT = "keyboard_rotation_timeout";
 
         /**
+         * Forces formal text input.  1 to replace emoticon key with enter key.
+         * @hide
+         */
+        public static final String FORMAL_TEXT_INPUT = "formal_text_input";
+
+        /**
          * Show the pending notification counts as overlays on the status bar
          * Whether to enable custom rebindings of the actions performed on
          * certain key press events.
