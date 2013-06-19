b92f2370beabc7210431a66d0bd65c0bc37f56dc
Danny Baumann <dannybaumann@web.de>
2013-06-19 12:51:08 +0200

    Close notification panel when swiping away the last clearable notification.
    Change-Id: I4e5114f66e799a7c80e18b38abce5678da460846

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1114,6 +1114,16 @@ public abstract class BaseStatusBar extends SystemUI implements
         updateExpansionStates();
         updateNotificationIcons();
 
+        if (entry.userCleared() && !mNotificationData.hasClearableItems()) {
+            // wait a bit to make the user aware of what's happening
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
+                }
+            }, 225);
+        }
+
         return entry.notification;
     }
 
