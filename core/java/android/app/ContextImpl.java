4075411ea07ded106ba6d72bb9e192d7eca11652
Timo Wendt <timo@tjwendt.de>
2013-06-22 23:04:00 +0200

    Irda: Add Irda System Service
    This is not a perfect solution yet, but it does work and should therefore be a starting point.
    It needs work to be functional on devices other than the Galaxy S4
    
    * The Irda service is not started by default. It can be enabled via overlay.
    * Uses a HAL module to communicate with sysfs
    
    Change-Id: If0c4b2e59144b1f199c44add399a3f528ac11e5b
    
    Conflicts:
    	core/java/android/app/ContextImpl.java
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/SystemServer.java

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -87,6 +87,8 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.IPowerManager;
 import android.os.IUserManager;
+import android.hardware.IIrdaManager;
+import android.hardware.IrdaManager;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.os.Process;
@@ -574,6 +576,13 @@ class ContextImpl extends Context {
                 IAppOpsService service = IAppOpsService.Stub.asInterface(b);
                 return new AppOpsManager(ctx, service);
             }});
+
+        registerService(IRDA_SERVICE, new StaticServiceFetcher() {
+                public Object createStaticService() {
+                    IBinder b = ServiceManager.getService(IRDA_SERVICE);
+                    IIrdaManager service = IIrdaManager.Stub.asInterface(b);
+                    return new IrdaManager(service);
+                }});
     }
 
     static ContextImpl getImpl(Context context) {
