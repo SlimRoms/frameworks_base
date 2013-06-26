c38f472c08614eab841699a4b6ffbe384169ee23
Danny Baumann <dannybaumann@web.de>
2013-06-26 10:55:05 +0200

    Make contacts app aware of blacklist being disabled (1/3)
    Change-Id: Ic59f370a6e78390c9b2b1510e0ec85b2f4af9146

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2025,6 +2025,12 @@ public final class Settings {
         public static final int VOLUME_OVERLAY_NONE = 3;
 
         /**
+         * Whether the blacklisting feature for phone calls is enabled
+         * @hide
+         */
+        public static final String PHONE_BLACKLIST_ENABLED = "phone_blacklist_enabled";
+
+        /**
          * Whether a notification should be shown when a call/message is blocked
          * @hide
          */
@@ -2832,6 +2838,7 @@ public final class Settings {
             VIBRATE_WHEN_RINGING,
             RINGTONE,
             NOTIFICATION_SOUND,
+            PHONE_BLACKLIST_ENABLED,
             PHONE_BLACKLIST_NOTIFY_ENABLED,
             PHONE_BLACKLIST_PRIVATE_NUMBER_MODE,
             PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE,
