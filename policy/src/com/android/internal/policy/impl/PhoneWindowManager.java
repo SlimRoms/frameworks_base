d0e317ee10e933d31021c69abb9f3323d2ab16fd
kaiyiz <kaiyiz@codeaurora.org>
2013-07-30 12:59:19 +0800

    Camera: make HW-Capture key can not waken up the screen
    When HW-Capture key is pressed,
    will generate KEYCODE_FOCUS key event which wakes up the screen.
    
    make KEYCODE_FOCUS key event not to wake up the screen.
    
    CRs-Fixed: 516900
    
    Change-Id: I53beb28b9981141b02fd9fc1223e97efd1ab400e

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4580,6 +4580,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_MEDIA_FAST_FORWARD:
             case KeyEvent.KEYCODE_MEDIA_AUDIO_TRACK:
             case KeyEvent.KEYCODE_CAMERA:
+            case KeyEvent.KEYCODE_FOCUS:
                 return false;
         }
         return true;
