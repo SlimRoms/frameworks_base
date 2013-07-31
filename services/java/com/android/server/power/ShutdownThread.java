fcfdf2ff287762275bf78d4bc766e58b5dd0a3ae
Der-Schubi <schubi@erlangen.ccc.de>
2013-07-31 09:19:14 -0300

    Advanced reboot enabled by default (1/2)
    Change-Id: Ie8ab34ecc532d14d296e33fa568e6cf3ebc681e8
    
    Conflicts:
    	services/java/com/android/server/power/ShutdownThread.java

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -223,7 +223,7 @@ public final class ShutdownThread extends Thread {
 
     private static int getAdvancedReboot(Context context) {
         return Settings.Secure.getInt(context.getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT, 0);
+                Settings.Secure.ADVANCED_REBOOT, 1) == 1;
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
