8a83ac711170a886f8d7851b631546ea32a6d41c
Lars Greiss <kufikugel@googlemail.com>
2013-09-05 19:23:15 -0700

    Privacy Guard Slim version (1/2)
    Re-implements slims ehanced privacy guard from 4.3
    and addapt it to 4.4 changes
    
    Change-Id: If57de2b541fa6b0a590fd65e8664bf173e259035

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -293,6 +293,11 @@ public final class ActivityStackSupervisor implements DisplayListener {
         }
     }
 
+    /**
+     * Is the privacy guard currently enabled? Shared between ActivityStacks
+     */
+    String mPrivacyGuardPackageName = null;
+
     public ActivityStackSupervisor(ActivityManagerService service) {
         mService = service;
         mHandler = new ActivityStackSupervisorHandler(mService.mHandler.getLooper());
