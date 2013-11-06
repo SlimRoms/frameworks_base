6229c15fa9c0b7421d04f46e259110b82ef4c3d1
Lars Greiss <kufikugel@googlemail.com>
2013-11-06 03:15:31 +0100

    Keaguard: enable translucent keyguard by default
    currently it is possible to enable it per device over an overlay and it is disabled
    by default. Enable it by default but respect general transluscent disable from
    device tree. Manta eg disables this feature due of graphical glichtes.
    
    Change-Id: I3579d47ee3bee05719ef357ffcf19c0137b2302f

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -146,7 +146,8 @@ public class KeyguardViewManager {
 
     private boolean shouldEnableTranslucentDecor() {
         Resources res = mContext.getResources();
-        return res.getBoolean(R.bool.config_enableLockScreenTranslucentDecor);
+        return res.getBoolean(R.bool.config_enableLockScreenTranslucentDecor)
+            && res.getBoolean(R.bool.config_enableTranslucentDecor);
     }
 
     class ViewManagerHost extends FrameLayout {
