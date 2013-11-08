645998f0e4b00055dc5182967cf62affa8cf82da
Lars Greiss <kufikugel@googlemail.com>
2013-11-08 20:28:53 +0100

    fb: volume rocker controll (wake up and volume) thanks to
    CM and omni for the proxy sensor idea
    
    Change-Id: I60f5f88e32a129afda80f5b0f4f41cb0a322b691

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2590,6 +2590,18 @@ public final class Settings {
         public static final String POWER_MENU_CONFIG = "power_menu_config";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+        /**
+         * Volume music controls
+         * @hide
+         */
+        public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
