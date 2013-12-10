865ee3ba8dab894a8b91b350ad11eb417a07e40c
Lars Greiss <kufikugel@googlemail.com>
2013-12-10 17:00:16 +0100

    fb: allow to recreate statusbar, navbar and notification drawer
    we need this for several upcoming features so add support for it.
    
    Change-Id: I3d46456c586c99ccb43c41477f606d95e760044e

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -63,6 +63,7 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
@@ -136,6 +137,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected PopupMenu mNotificationBlamePopup;
 
     protected int mCurrentUserId = 0;
+
+    protected FrameLayout mStatusBarContainer;
 
     protected int mLayoutDirection = -1; // invalid
     private Locale mLocale;
@@ -248,6 +251,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         mLocale = mContext.getResources().getConfiguration().locale;
         mLayoutDirection = TextUtils.getLayoutDirectionFromLocale(mLocale);
+
+        mStatusBarContainer = new FrameLayout(mContext);
 
         // Connect in to the status bar manager service
         StatusBarIconList iconList = new StatusBarIconList();
