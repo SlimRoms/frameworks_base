11c611fa5c56288f0ec39811518f4d0341cc4cb9
Lars Greiss <kufikugel@googlemail.com>
2013-12-13 19:00:05 +0100

    QS: enable haptic feedback and change Alarm tile longpress
    nice idea from xploid to have haptic feedback on the tiles
    just a bit to complicated to do on every tile
    
    so activate it centralized
    
    as well change the longressbehaviour of alarm tile to the
    normal onClick behaviour
    
    Change-Id: I48d4129c63ea6396b2a176bee9d6df02d414d28a

--- packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -28,6 +28,7 @@ import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.TypedValue;
+import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -148,6 +149,7 @@ public class QuickSettingsTile implements OnClickListener {
     public void onClick(View v) {
         if (mOnClick != null) {
             mOnClick.onClick(v);
+            v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
         }
     }
 }
