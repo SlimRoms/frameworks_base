d261ab696c33aa583f9076ae38199286463f2c28
Lars Greiss <kufikugel@googlemail.com>
2013-12-14 02:52:23 +0100

    fb: TRDS 3.0 and new theme engine preparation (1/4)
    For the one who do not know this feature see as reference
    the first two versions here:
    
    https://github.com/SlimRoms/frameworks_base/commit/b330fde28306286572ba390e451a72539c0c3576
    
    https://github.com/SlimRoms/frameworks_base/commit/3194015737b7944b4185fca007b8d167a0675cd8
    
    we got now over a year now a lot experience how to handle the qualifier fallback
    as well how to theme it and how to handle other apps. Based on this knowledge we
    redid some parts of TRDS engine with following new features/additions and fixes
    to have now a final perfect working TRDS
    
    - compared to the old versions where it was needed on some apps to restart them
      in background this is not needed any more. All is handled about the internal
      AOSP resource handling and on qualifier change the caches are cleared
      and the apps get an onconfigurationchanged to restart the activity
      like it is doing on orientation change eg
    - inverted folder is now depreciated and the new folders are holodark
      and hololight which allows on the one hand a dark theme (at least
      how TRDS started) and a light based theme hololight
    - This name change is mainly because of the new theme engine which
      will come which uses this as base to add 2 external themes at the same
      time and give the user the ability to switch between them. But this is
      still in early stages and this commit is just the preparation for it
    - Two new automodes are now integrated. User can enable the twilight modus
      which enables/disables holodark/hololight depending on the daytime...means
      on the evening it switches to holodark on normal day it is hololight.
      The other automode is based on light conditions....means that if it is
      dark outside it switches to holodark....if we have bright light conditions
      it switches immediatelly to hololight. Due of the resource cache handling now
      this happens immediatelly for the user and feels completly system integrated
      and happens without delay. A bit comparable to some navigation tool for cars
      which can switch automatically from dark to light and back
    - currently the auto modes as well the manual mode switches between holodark
      and hololight. Due that we do not have currently anything hololight themed
      in the source hololight = stock theme. We will give the user the ability
      later when done to choose between which modes the user want to switch
      (stock/holodark/hololight). But this does not make sense at the moment.
      As soon as we have done the light theme or theme engine (lets see what
      comes first) we will open the ability to choose it. This is only a small code change
    - a lot small tweaks and fixes went as well into the new version
    
    Whatever this commit as usual adds the support for it. The themeing is done in the
    other commits by kroz which is as well way more extended then on the older versions
    
    cheers
    
    PatchSets:
    - minor code cleanup
    
    PatchSet5:
    - use one value for auto modes
    
    PatchSet6:
    - kill resource caches on configuration change
    
    PatchSet7:
    - register settings observer lol
    
    PatchSet8:
    - fix not assigment of config mode change (typo lol)
    
    PatchsSet 9 -11:
    - playing around aosp qualifier is not that flexible then I thought
    whatever back in most parts to original implementation handling
    
    PatchSet 12:
    - now no restarts of any application needed any more like it was on 4.3...yeaaah
    - fix wrong shifting in native adapter
    - fix wrong call of twilightmode
    - disable debug
    - cleanup
    
    PatchSet 13:
    - fix lightsensor
    
    Patchset 14:
    - no need currently to recreate the navbar we can do if a theme needs it
    
    Patchset 16:
    - synchronize on sending config change
    - only send if system is ready
    - fire first update when system is ready
    
    -rebase
    
    Change-Id: Ic88838247c47d2a92b55d1cb1b00330f2d62fb88

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -194,6 +194,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     int mIconHPadding = -1;
     Display mDisplay;
     Point mCurrentDisplaySize = new Point();
+    int mCurrUiThemeMode;
     private float mHeadsUpVerticalOffset;
     private int[] mPilePosition = new int[2];
 
@@ -663,6 +664,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mDisplay = ((WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE))
                 .getDefaultDisplay();
         updateDisplaySize();
+
+        mCurrUiThemeMode = mContext.getResources().getConfiguration().uiThemeMode;
 
         super.start(); // calls createAndAddWindows()
 
@@ -3446,6 +3449,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     void updateResources() {
         final Context context = mContext;
         final Resources res = context.getResources();
+
+        // detect theme ui mode change
+        int uiThemeMode = res.getConfiguration().uiThemeMode;
+        if (uiThemeMode != mCurrUiThemeMode) {
+            mCurrUiThemeMode = uiThemeMode;
+            recreateStatusBar(false);
+            return;
+        }
 
         if (mClearButton instanceof TextView) {
             ((TextView)mClearButton).setText(context.getText(R.string.status_bar_clear_all_button));
