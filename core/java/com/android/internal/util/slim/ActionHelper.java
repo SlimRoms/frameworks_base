bb2ae0a8054ef542e3dc2ca09f397670f79d07d2
Lars Greiss <kufikugel@googlemail.com>
2013-12-14 02:52:23 +0100

    fb: TRDS 5.1 (1/4)
    an original slim feature
    
    Here's how TRDS switching works:
    If -lighttheme isn't present, switching will be between normal
    and -darktheme. If -darktheme isn't present, switching will be
    between -lighttheme and normal. In the case that both
    -lighttheme and -darktheme are present, switching will take
    place between both of them, however you can reset to standard
    resources (neither -lighttheme or -darktheme) by either long-
    pressing the TRDS quick-settings tile, or by tapping the theme
    reset button at the upper-right corner of the TRDS settings
    screen.  This will, in effect, allow you to have a triple-
    themed setup that you can switch between on-the-fly.
    
    The only resources that need to be placed in the new directories
    are the new ones that you want to change with TRDS switch. The
    rest of the resources will come from the standard resource
    directories.
    
    TRDS resource directories are named with -lighttheme and
    -darktheme qualifiers, and the only qualifiers that should ever
    come after these are the API qualifier (-v21, -v17, etc.) and
    the density qualifier (-xxhdpi, -nodpi, etc.)
    
    Here are some examples of resource directory names to give you
    an idea of the proper placement of the new qualifier:
    
    color-darktheme
    drawable-darktheme-xhdpi
    layout-darktheme
    layout-darktheme-v21
    layout-land-darktheme-v19
    values-darktheme
    values-darktheme-v21
    values-sw720dp-darktheme
    values-sw600dp-land-darktheme
    
    patchset: bring up tile usage tracker to 5.1 method
    
    patchset: fix usage tracker for TRDS tile
    
    patchset: correct icon resource for TRDS tile
    
    patchset: remove -darktheme resources from this commit
            (since many resources will eventually depend on
            rro nomenclature, they should exist in a separate
            commit)
    
    patchset: rebase
    
    patchset: fix build errors
    
    patchset: rebase on Dodd rro
    Change-Id: Ic88838247c47d2a92b55d1cb1b00330f2d62fb88

--- core/java/com/android/internal/util/slim/ActionHelper.java
+++ core/java/com/android/internal/util/slim/ActionHelper.java
@@ -312,6 +312,9 @@ public class ActionHelper {
         } else if (clickAction.equals(ActionConstants.ACTION_TORCH)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_torch", null, null);
+        } else if (clickAction.equals(ActionConstants.ACTION_THEME_SWITCH)) {
+            resId = systemUiResources.getIdentifier(
+                        SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_theme_switch", null, null);
         } else {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_null", null, null);
