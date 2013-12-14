430dcb6bbc7ea7cacea028b166ecd16a4370fb73
Daniel Koman <dankoman30@gmail.com>
2013-12-14 02:52:23 +0100

    fb: TRDS 5.1 (1/4)
    an original slim feature
    
    patchset: bring up tile usage tracker to 5.1 method
    
    patchset: fix usage tracker for TRDS tile
    
    patchset: correct icon resource for TRDS tile
    
    patchset: remove -darktheme resources from this commit
            (since many resources will eventually depend on
            rro nomenclature, they should exist in a separate
            commit)
    Change-Id: Ic88838247c47d2a92b55d1cb1b00330f2d62fb88

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.TrdsTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -292,6 +293,7 @@ public class QSTileHost implements QSTile.Host {
                 == TelephonyManager.MultiSimVariants.DSDA))
             return new DdsTile(this);
         else if (tileSpec.equals("apn")) return new ApnTile(this);
+        else if (tileSpec.equals("trds")) return new TrdsTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
