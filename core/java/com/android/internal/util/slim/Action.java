afcd1d238a5c95fcacfa1128dfa2e84b24eb0715
Daniel Koman <dankoman30@gmail.com>
2013-12-14 02:52:23 +0100

    fb: TRDS 5.1 (1/4)
    an original slim feature
    Change-Id: Ic88838247c47d2a92b55d1cb1b00330f2d62fb88

--- core/java/com/android/internal/util/slim/Action.java
+++ core/java/com/android/internal/util/slim/Action.java
@@ -19,8 +19,10 @@ package com.android.internal.util.slim;
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.SearchManager;
+import android.app.IUiModeManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.Intent;
 import android.hardware.input.InputManager;
 import android.media.AudioManager;
@@ -40,6 +42,7 @@ import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
+import android.widget.Toast;
 
 import java.net.URISyntaxException;
 
@@ -65,6 +68,21 @@ public class Action {
                         WindowManagerGlobal.getWindowManagerService().isKeyguardLocked();
             } catch (RemoteException e) {
                 Log.w("Action", "Error getting window manager service", e);
+            }
+
+            IStatusBarService barService = IStatusBarService.Stub.asInterface(
+                    ServiceManager.getService(Context.STATUS_BAR_SERVICE));
+            if (barService == null) {
+                return; // ouch
+            }
+
+            if (collapseShade) {
+                if (!action.equals(ActionConstants.ACTION_THEME_SWITCH)) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
+                }
             }
 
             // process the actions
@@ -209,6 +227,36 @@ public class Action {
                         (PowerManager) context.getSystemService(Context.POWER_SERVICE);
                 if (!powerManager.isScreenOn()) {
                     powerManager.wakeUp(SystemClock.uptimeMillis());
+                }
+                return;
+            } else if (action.equals(ActionConstants.ACTION_THEME_SWITCH)) {
+                boolean autoLightMode = Settings.Secure.getIntForUser(
+                        context.getContentResolver(),
+                        Settings.Secure.UI_THEME_AUTO_MODE, 0,
+                        UserHandle.USER_CURRENT) == 1;
+                boolean state = context.getResources().getConfiguration().uiThemeMode
+                        == Configuration.UI_THEME_MODE_HOLO_DARK;
+                if (autoLightMode) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
+                    Toast.makeText(context,
+                            com.android.internal.R.string.theme_auto_switch_mode_error,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Handle a switch change
+                // we currently switch between darktheme and lighttheme till either
+                // theme engine is ready or lighttheme is ready. Currently due of
+                // missing light themeing lighttheme = system base theme
+                final IUiModeManager uiModeManagerService = IUiModeManager.Stub.asInterface(
+                        ServiceManager.getService(Context.UI_MODE_SERVICE));
+                try {
+                    uiModeManagerService.setUiThemeMode(state
+                            ? Configuration.UI_THEME_MODE_HOLO_LIGHT
+                            : Configuration.UI_THEME_MODE_HOLO_DARK);
+                } catch (RemoteException e) {
                 }
                 return;
             } else {
