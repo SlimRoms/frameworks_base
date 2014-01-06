32f9a28b3292e4a5a5a839a86fbeabb6ef57772e
Sasikumar D R <sasikumardr@gmail.com>
2014-01-07 02:41:17 +0530

    [1/3] Flip to Mute/Reject Call
    saving flip action value to Setings container
    
    Change-Id: Ie72dda49d78b23b9bc7d8a886f6916f678c918d1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3247,6 +3247,14 @@ public final class Settings {
         public static final String CALL_UI_IN_BACKGROUND = "call_ui_in_background";
 
         /**
+         * Whether flip action during incomming call should mute or dismiss
+         * the call (mute = 0, dismiss = 1, nothing = 2 (default))
+         *
+         * @hide
+         */
+        public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
+
+        /**
          * Enable Stylus Gestures
          *
          * @hide
