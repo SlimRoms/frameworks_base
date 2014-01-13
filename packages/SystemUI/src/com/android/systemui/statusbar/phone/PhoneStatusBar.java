8ab0b28682821e519c65ab5d9944194701a1393e
Muhammed Nazim <mnm9994u@gmail.com>
2014-01-13 17:37:23 +0100

    Frameworks: Pimp up Notification Labels Hide [1/2]
    This adds fine grain priorities to hide the labels in the notification
    drawer in the following ways:
    
    - Show all Labels
    - Hide Carrier Label
    - Hide WiFI Lable
    - Hide all Labels
    
    PS:
    - fix codestyle
    
    Change-Id: I8be92bdf27ba017b6aec3c2b311a83758cded6ff

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -261,7 +261,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     private TextView mEmergencyCallLabel;
     private int mNotificationHeaderHeight;
 
-    private boolean mShowCarrierInPanel = false;
+    private int mHideLabels;
 
     private boolean mRecreating = false;
 
@@ -377,7 +377,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                     Settings.System.NOTIFICATION_SHORTCUTS_CONFIG),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.NOTIFICATION_HIDE_CARRIER),
+                    Settings.System.NOTIFICATION_HIDE_LABELS),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_BATTERY),
@@ -573,11 +573,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                     || notificationShortcutsIsActive.isEmpty());
 
             if (mCarrierLabel != null) {
-                mShowCarrierInPanel = Settings.System.getIntForUser(resolver,
-                        Settings.System.NOTIFICATION_HIDE_CARRIER, 0, UserHandle.USER_CURRENT) == 0;
-                updateCarrierMargin(!mShowCarrierInPanel);
+                mHideLabels = Settings.System.getIntForUser(resolver,
+                        Settings.System.NOTIFICATION_HIDE_LABELS, 0, UserHandle.USER_CURRENT);
+                updateCarrierMargin(mHideLabels == 3);
                 mCarrierAndWifiView.setVisibility(
-                        mShowCarrierInPanel ? View.VISIBLE : View.INVISIBLE);
+                        (mHideLabels != 3) ? View.VISIBLE : View.INVISIBLE);
             }
             updateBatteryIcons();
         }
@@ -911,11 +911,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mWifiView = mStatusBarWindow.findViewById(R.id.wifi_view);
         mCarrierLabel = (TextView)mStatusBarWindow.findViewById(R.id.carrier_label);
         if (mCarrierLabel != null) {
-            mShowCarrierInPanel = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.NOTIFICATION_HIDE_CARRIER, 0, UserHandle.USER_CURRENT) == 0;
+            mHideLabels = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.NOTIFICATION_HIDE_LABELS, 0, UserHandle.USER_CURRENT);
             lpCarrierLabel = (FrameLayout.LayoutParams) mCarrierAndWifiView.getLayoutParams();
-            mCarrierLabel.setVisibility((mCarrierAndWifiViewVisible && mShowCarrierInPanel)
-                    ? View.VISIBLE : View.INVISIBLE);
+            mCarrierLabel.setVisibility(View.VISIBLE);
+
             // for mobile devices, we always show mobile connection info here (SPN/PLMN)
             // for other devices, we show whatever network is connected
             if (mNetworkController.hasMobileDataFeature()) {
@@ -1591,7 +1591,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     }
 
     protected void updateCarrierAndWifiLabelVisibility(boolean force, boolean forceHide) {
-        if (!mShowCarrierInPanel || mCarrierAndWifiView == null) {
+        if ((mHideLabels == 3) || mCarrierAndWifiView == null) {
             return;
         }
         // The idea here is to only show the carrier label when there is enough room to see it,
