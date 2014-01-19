5d73e9280f8644835bf12c2d4fb6841b23394c2a
Lars Greiss <kufikugel@googlemail.com>
2014-01-19 08:00:36 +0100

    fb: Keyguard disable challenge drag down on slimpin/simpuk screen
    On simpin, simpuck and as well account change screen it does not make
    any sense to give the user the abillity to drag down the challenge to show
    the widget maximized (neither if widgets are enabled or disabled)
    Reason: simpin/simpuck widget is not shown due of security reasons
    and on account change the next expected behaviour is that the user gets a new
    setup. So it does not make in this case as well sense
    
    Just check for current securityscreen selection to block this behaviour. As
    well move the enable/disable code to a proper place
    
    Change-Id: I0b16d1e590252b537d6c71c4aa9b133ed832be45

--- packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
@@ -461,12 +461,6 @@ public class KeyguardHostView extends KeyguardViewBase {
         maybeEnableAddButton();
         checkAppWidgetConsistency();
 
-        // Don't let the user drag the challenge down if widgets are disabled.
-        if (mSlidingChallengeLayout != null) {
-            mSlidingChallengeLayout.setEnableChallengeDragging(
-                    !widgetsDisabled() || mDefaultAppWidgetAttached);
-        }
-
         // Select the appropriate page
         mSwitchPageRunnable.run();
 
@@ -1090,6 +1084,11 @@ public class KeyguardHostView extends KeyguardViewBase {
 
         if (mSlidingChallengeLayout != null) {
             mSlidingChallengeLayout.setChallengeInteractive(!fullScreenEnabled);
+            // Don't let the user drag the challenge down if widgets are disabled
+            // or it is a simpin, simpuk or accountswitcher lockscreen
+            mSlidingChallengeLayout.setEnableChallengeDragging(
+                    !isSimOrAccount(securityMode, false)
+                    && (!widgetsDisabled() || mDefaultAppWidgetAttached));
         }
 
         // Emulate Activity life cycle
