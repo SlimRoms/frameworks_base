aadb043f37ea67cfcc9e6236de12e8f2a6102ea7
Jubakuba <Jubakuba@gmail.com>
2014-02-09 20:58:32 -0700

    Frameworks: Notification Reminders (1/2)
    Click notification header to set a reminder
    Long-click to dismiss current
    
    Change-Id: I68d6eab8893419e8ec192d45f1e6b42ddff3e62b

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -13,6 +13,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
     src/com/android/systemui/EventLogTags.logtags
 
 LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
 
 # unbundled
 LOCAL_STATIC_JAVA_LIBRARIES := \
