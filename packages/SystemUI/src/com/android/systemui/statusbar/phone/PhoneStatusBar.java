5d98f4861a8890a8db3368ac1c69fa876c7129af
sdroyds <mike3582.mw@gmail.com>
2014-03-03 09:53:27 -0500

    Twisted version of The new Recent Apps

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3611,6 +3611,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         repositionNavigationBar();
 
+        rebuildRecentsScreen();
+
         // recreate StatusBarIconViews.
         for (int i = 0; i < nIcons; i++) {
             StatusBarIcon icon = icons.get(i);
