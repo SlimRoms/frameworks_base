eac28f37904453b899685846032dfa4d1e55f249
Jubakuba <Jubakuba@gmail.com>
2014-03-12 08:16:38 -0600

    Frameworks: More Keyguard Colors (1/2)
    No content resolver?  Colors EVERYWHERE!!!
    
    PS2: Code clean-up && fix avatar
    	text color && set avatar
    	border color (AOSP was never
    	using their own color value
    	either <_< so fix that as well
    
    Change-Id: Ie4daaf1588cd2bbff8871c2beea862f2a15faef7

--- packages/Keyguard/src/com/android/keyguard/CarrierText.java
+++ packages/Keyguard/src/com/android/keyguard/CarrierText.java
@@ -17,6 +17,7 @@
 package com.android.keyguard;
 
 import android.content.Context;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.method.SingleLineTransformationMethod;
 import android.text.TextUtils;
@@ -84,6 +85,13 @@ public class CarrierText extends TextView {
         mLockPatternUtils = new LockPatternUtils(mContext);
         boolean useAllCaps = mContext.getResources().getBoolean(R.bool.kg_use_all_caps);
         setTransformationMethod(new CarrierTextTransformationMethod(mContext, useAllCaps));
+        int textColor = Settings.Secure.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.Secure.LOCKSCREEN_MISC_COLOR, -2,
+                UserHandle.USER_CURRENT);
+        if (textColor != -2) {
+            setTextColor(textColor);
+        }
     }
 
     protected void updateCarrierText(State simState, CharSequence plmn, CharSequence spn) {
