a8b7dd73e2c992b6a7cf910595ca5f51436d3369
Lars Greiss <kufikugel@googlemail.com>
2014-03-17 03:10:25 +0100

    Revert "Don't recreate keyguard for MCC/MNC changes."
    This reverts commit 28dcece574df81bc60f6d03b7c06a8783a80bee2.
    
    Change-Id: I14b67ba7d2e7116546b04cf6ebbcfb734f48cbb4

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -159,7 +159,6 @@ public class KeyguardViewManager {
         private static final int BACKGROUND_COLOR = 0x70000000;
 
         private Drawable mCustomBackground;
-        private Configuration mLastConfiguration;
 
         // This is a faster way to draw the background on devices without hardware acceleration
         private final Drawable mBackgroundDrawable = new Drawable() {
@@ -187,7 +186,6 @@ public class KeyguardViewManager {
         public ViewManagerHost(Context context) {
             super(context);
             setBackground(mBackgroundDrawable);
-            mLastConfiguration = context.getResources().getConfiguration();
         }
 
         public void drawToCanvas(Canvas canvas, Drawable drawable) {
@@ -276,16 +274,12 @@ public class KeyguardViewManager {
         @Override
         protected void onConfigurationChanged(Configuration newConfig) {
             super.onConfigurationChanged(newConfig);
-            int diff = newConfig.diff(mLastConfiguration);
-            if ((diff & ~(ActivityInfo.CONFIG_MCC | ActivityInfo.CONFIG_MNC)) == 0) {
-                if (DEBUG) Log.v(TAG, "onConfigurationChanged: no relevant changes");
-            } else if (mKeyguardHost.getVisibility() == View.VISIBLE) {
+            if (mKeyguardHost.getVisibility() == View.VISIBLE) {
                 // only propagate configuration messages if we're currently showing
                 maybeCreateKeyguardLocked(shouldEnableScreenRotation(), true, null);
             } else {
                 if (DEBUG) Log.v(TAG, "onConfigurationChanged: view not visible");
             }
-            mLastConfiguration = newConfig;
         }
 
         @Override
