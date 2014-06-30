3f9d15d71c6e84523d57a1b7407bd497bdcc863b
Jubakuba <Jubakuba@gmail.com>
2014-06-30 11:59:58 -0600

    Framework: Rewrite Quiet Hours (2/3)
    Added:
    	New Toggle States.
    		Toggle is now aware
    		of if quiet hours
    		is truly enabled or not.
    		Enable "force" mode
    		on click: turns quiet hours
    		on / off
    		Double-click to return
    		to "timed" mode.
    
    	New Features:
    		Wifi Connected Requirement
    		Battery Charging Requirement
    		Individual day times
    
            Completely rewrote quiet hours
                    to reduce system calls.
                    No more calendar objects
                    or checking three preferences
                    and comparing their values
                    on each quiet hours check.
                    Reduce that to ONE preference
                    check per preference
    
    PS2: Ensure booted before firing intent
    
    Change-Id: I61ef5afa32c5ff883129b9362423af7f724c5ac1

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -80,7 +80,6 @@ import android.widget.Toast;
 import com.android.internal.R;
 
 import com.android.internal.notification.NotificationScorer;
-import com.android.internal.util.slim.QuietHoursHelper;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -1294,15 +1293,6 @@ public class NotificationManagerService extends INotificationManager.Stub
                     Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_ENABLED),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_START),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_END),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QUIET_HOURS_MUTE),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -1951,18 +1941,16 @@ public class NotificationManagerService extends INotificationManager.Stub
 
                         Uri soundUri = null;
                         boolean hasValidSound = false;
-
-                        if (!(QuietHoursHelper.inQuietHours(
-                                    mContext, Settings.System.QUIET_HOURS_MUTE))
-                                && useDefaultSound) {
+                        ContentResolver resolver = mContext.getContentResolver();
+                        final boolean quietHoursMute = Settings.System.getInt(resolver,
+                                   Settings.System.QUIET_HOURS_MUTE, 0) == 2;
+                        if (!quietHoursMute && useDefaultSound) {
                             soundUri = Settings.System.DEFAULT_NOTIFICATION_URI;
 
                             // check to see if the default notification sound is silent
-                            ContentResolver resolver = mContext.getContentResolver();
                             hasValidSound = Settings.System.getString(resolver,
                                    Settings.System.NOTIFICATION_SOUND) != null;
-                        } else if (!(QuietHoursHelper.inQuietHours(mContext,
-                                Settings.System.QUIET_HOURS_MUTE)) && notification.sound != null) {
+                        } else if (!quietHoursMute && notification.sound != null) {
                             soundUri = notification.sound;
                             hasValidSound = (soundUri != null);
                         }
@@ -2009,8 +1997,8 @@ public class NotificationManagerService extends INotificationManager.Stub
                         final boolean useDefaultVibrate =
                                 (notification.defaults & Notification.DEFAULT_VIBRATE) != 0;
 
-                        if (!(QuietHoursHelper.inQuietHours(
-                                    mContext, Settings.System.QUIET_HOURS_STILL))
+                        if (Settings.System.getInt(resolver,
+                                   Settings.System.QUIET_HOURS_STILL, 0) != 2
                                 && (useDefaultVibrate || convertSoundToVibration || hasCustomVibrate)
                                 && !(audioManager.getRingerMode()
                                         == AudioManager.RINGER_MODE_SILENT)) {
@@ -2366,7 +2354,8 @@ public class NotificationManagerService extends INotificationManager.Stub
         // Don't flash while we are in a call, screen is
         // on or we are in quiet hours with light dimmed
         if (mLedNotification == null || mInCall || mScreenOn
-                || (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM))) {
+                || Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_DIM, 0) == 2) {
             mNotificationLight.turnOff();
         } else if (mNotificationPulseEnabled) {
             final Notification ledno = mLedNotification.sbn.getNotification();
