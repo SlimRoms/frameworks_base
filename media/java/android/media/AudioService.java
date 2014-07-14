a0d25e375a97ec2ed1b676b7012bd6f9e3c549a9
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-07-14 17:13:21 +0100

    AudioService: Show safe volume panel for non-interactive volume adjustments
    If safe volume is active, apps calling mp.setVolume() to adjust the
    headset to a value higher than the threshold will have the operation
    fail silently because no volume dialog is active to pop up the
    "do you wish to override" question.
    This is particularly bad for peripherals that use the audio jack
    (Square readers, Jawbone bands), since they usually require levels
    close to maximum for proper communication and the app isn't prepared
    to deal with a silent rejection of the requested volume.
    
    Change-Id: I2047d199506106a87841b8b680007c5ab45b527b

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -1082,7 +1082,7 @@ public class AudioService extends IAudioService.Stub {
             }
 
             if (!checkSafeMediaVolume(streamTypeAlias, index, device)) {
-                mVolumePanel.postDisplaySafeVolumeWarning(flags);
+                mVolumePanel.postDisplaySafeVolumeWarning(flags | AudioManager.FLAG_SHOW_UI);
                 mPendingVolumeCommand = new StreamVolumeCommand(
                                                     streamType, index, flags, device);
             } else {
