5383b4fe1a20d521101126592302551f9c038317
Lars Greiss <kufikugel@googlemail.com>
2014-07-27 04:01:49 +0200

    SlimActions: add new actions and cleanup
    A bit cleanup.
    
    As well add 4 actions:
    
    - camera intent
    - play/pause music
    - next music track
    - prev music track
    - wake up device
    
    As well check for system ready on the other audio actions which are already present.
    
    ToDo: One of the rare classes which are very bad documented. We need to add code comments
    ASAP
    
    Change-Id: I73bcfb6c60454de4d8494dfd846deee770022335

--- core/java/com/android/internal/util/slim/ButtonsConstants.java
+++ core/java/com/android/internal/util/slim/ButtonsConstants.java
@@ -50,6 +50,12 @@ public class ButtonsConstants {
     public static final String ACTION_IME_NAVIGATION_RIGHT = "**ime_nav_right**";
     public static final String ACTION_IME_NAVIGATION_UP    = "**ime_nav_up**";
     public static final String ACTION_IME_NAVIGATION_DOWN  = "**ime_nav_down**";
+    public static final String ACTION_CAMERA               = "**camera**";
+    public static final String ACTION_MEDIA_PREVIOUS       = "**media_previous**";
+    public static final String ACTION_MEDIA_NEXT           = "**media_next**";
+    public static final String ACTION_MEDIA_PLAY_PAUSE     = "**media_play_pause**";
+    public static final String ACTION_WAKE_DEVICE          = "**wake_device**";
+
 
     // no action
     public static final String ACTION_NULL            = "**null**";
