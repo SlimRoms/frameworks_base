a99baf3c35d9ada950bc44e5d5eb12995fda40b8
Danesh M <daneshm90@gmail.com>
2014-08-04 18:09:06 -0700

    Proximity : Check proximity flags only if from native
    Change-Id: Id1be619e638de6a45252d8390eab6e38bd5de5a7

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -1084,7 +1084,11 @@ public final class PowerManagerService extends IPowerManager.Stub
                 }
             }
         };
-        runWithProximityCheck(r);
+        if (checkProximity) {
+            runWithProximityCheck(r);
+        } else {
+            r.run();
+        }
     }
 
     private void runWithProximityCheck(Runnable r) {
