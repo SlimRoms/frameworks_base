a69847dca997aef3a59a134342596eee07723b4e
Shashank Mittal <mittals@codeaurora.org>
2014-08-07 21:58:14 -0700

    AppOPs: add delete ops for some operations
    Delete operation is required to be seprated from modify
    operation. Add delete ops for sms, mms, contacts, and
    call log.
    
    Change-Id: I19ca4757884ba31a05b403a092796cc03042d31c

--- core/java/android/content/ContentProvider.java
+++ core/java/android/content/ContentProvider.java
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Copyright (C) 2006 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -304,7 +307,7 @@ public abstract class ContentProvider implements ComponentCallbacks2 {
         public int delete(String callingPkg, Uri uri, String selection, String[] selectionArgs) {
             validateIncomingUri(uri);
             uri = getUriWithoutUserId(uri);
-            if (enforceWritePermission(callingPkg, uri, null) != AppOpsManager.MODE_ALLOWED) {
+            if (enforceDeletePermission(callingPkg, uri, null) != AppOpsManager.MODE_ALLOWED) {
                 return 0;
             }
             final String original = setCallingPackage(callingPkg);
@@ -461,6 +464,32 @@ public abstract class ContentProvider implements ComponentCallbacks2 {
             enforceWritePermissionInner(uri, callerToken);
             if (mWriteOp != AppOpsManager.OP_NONE) {
                 return mAppOpsManager.noteOp(mWriteOp, Binder.getCallingUid(), callingPkg);
+            }
+            return AppOpsManager.MODE_ALLOWED;
+        }
+
+        private int enforceDeletePermission(String callingPkg, Uri uri, IBinder callerToken)
+                throws SecurityException {
+            enforceWritePermissionInner(uri, callerToken);
+            if (mWriteOp != AppOpsManager.OP_NONE) {
+                int op = mWriteOp;
+                switch (mWriteOp) {
+                case AppOpsManager.OP_WRITE_SMS:
+                    op = AppOpsManager.OP_DELETE_SMS;
+                    break;
+                case AppOpsManager.OP_WRITE_MMS:
+                    op = AppOpsManager.OP_DELETE_MMS;
+                    break;
+                case AppOpsManager.OP_WRITE_CONTACTS:
+                    op = AppOpsManager.OP_DELETE_CONTACTS;
+                    break;
+                case AppOpsManager.OP_WRITE_CALL_LOG:
+                    op = AppOpsManager.OP_DELETE_CALL_LOG;
+                    break;
+                default:
+                    break;
+                }
+               mAppOpsManager.noteOp(op, Binder.getCallingUid(), callingPkg);
             }
             return AppOpsManager.MODE_ALLOWED;
         }
