d968f38434c6d4f696053fc81d1b8353975b3202
kaiyiz <kaiyiz@codeaurora.org>
2014-08-14 12:33:54 +0800

    IME: Fix Happened IllegalArgumentException 315 times during monkey test
    The method startInputInnerLocked() sometimes throw IllegalArgument
    Exception. There are 3 places invoked this method, but one of them
    forgot to catch this exception.
    
    Catch the IllegalArgumentException when invoke startInputInnerLocked().
    
    CRs-Fixed: 563388
    
    Change-Id: I315cb46b1d16c2862989cb9c0c10ed6613884522

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -1249,7 +1249,12 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             }
         }
 
-        return startInputInnerLocked();
+        try {
+            return startInputInnerLocked();
+        } catch (RuntimeException e) {
+            Slog.w(TAG, "Unexpected exception", e);
+            return null;
+        }
     }
 
     InputBindResult startInputInnerLocked() {
