e1932cb5cdf0d75e226d4f82bc930d660aaae136
dankoman <dankoman30@gmail.com>
2014-09-05 23:29:14 +0200

    FB: Heads Up background color && transparency [2/2]
    PS: manual rebase
    
    PS: fix my retardedness
    
    PS: fix heads up TRDS background resources
    
    PS: clear content holder background prior to setting TRDS
            drawable so that correct drawable for TRDS state
            is set
    
    PS: revert non-TRDS background to AOSP gray (default
            system notification background color)
    
    PS: clean up some unused code
    
    PS: add capability to set custom heads up text color
    Change-Id: I3942c0ce178491e3429e5ae7eb24b43ba07a1f40

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3550,6 +3550,18 @@ public final class Settings {
         public static final String HEADS_UP_SHOW_UPDATE = "heads_up_show_update";
 
         /**
+         * Heads Up background color
+         * @hide
+         */
+        public static final String HEADS_UP_BG_COLOR = "heads_up_bg_color";
+
+        /**
+         * Heads Up text color
+         * @hide
+         */
+        public static final String HEADS_UP_TEXT_COLOR = "heads_up_text_color";
+
+        /**
          * Sensitivity of all system shake events
          * @hide
          */
