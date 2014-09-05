25b6f90cdd0ef9ad1e26a70c01723b27caec45b2
dankoman <dankoman30@gmail.com>
2014-09-05 23:29:14 +0200

    FB: Heads Up background color && transparency [2/2]
    PS: manual rebase
    
    PS: fix my retardedness
    
    PS: fix heads up TRDS background resources
    
    PS: clear content holder background prior to setting TRDS
            drawable so that correct drawable for TRDS state
            is set
    
    PS: revert non-TRDS background to AOSP gray (default
            system notification background color)
    
    PS: clean up some unused code
    
    PS: add capability to set custom heads up text color
    Change-Id: I3942c0ce178491e3429e5ae7eb24b43ba07a1f40

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -158,6 +158,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     private long mHeadsUpSnoozeStartTime;
     protected String mHeadsUpPackageName;
     protected boolean mShowHeadsUpUpdates;
+    private int mHeadsUpTextColor;
 
     protected IDreamManager mDreamManager;
     PowerManager mPowerManager;
@@ -807,7 +808,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected void onShowSearchPanel() {
     }
 
-    public boolean inflateViews(NotificationData.Entry entry, ViewGroup parent) {
+    public boolean inflateViews(NotificationData.Entry entry,
+            ViewGroup parent, int customTextColor) {
         int minHeight =
                 mContext.getResources().getDimensionPixelSize(R.dimen.notification_min_height);
         int maxHeight =
@@ -815,8 +817,29 @@ public abstract class BaseStatusBar extends SystemUI implements
         StatusBarNotification sbn = entry.notification;
         RemoteViews contentView = sbn.getNotification().contentView;
         RemoteViews bigContentView = sbn.getNotification().bigContentView;
+        mHeadsUpTextColor = customTextColor;
         if (contentView == null) {
             return false;
+        }
+        if (mHeadsUpTextColor != 0) {
+            if (contentView != null) {
+                contentView.setTextColor(com.android.internal.R.id.title, mHeadsUpTextColor);
+                contentView.setTextColor(com.android.internal.R.id.text, mHeadsUpTextColor);
+                contentView.setTextColor(com.android.internal.R.id.big_text, mHeadsUpTextColor);
+                contentView.setTextColor(com.android.internal.R.id.time, mHeadsUpTextColor);
+//                contentView.setTextColor(com.android.internal.R.id.action0, mHeadsUpTextColor);
+                contentView.setTextColor(com.android.internal.R.id.text2, mHeadsUpTextColor);
+                contentView.setTextColor(com.android.internal.R.id.info, mHeadsUpTextColor);
+            }
+            if (bigContentView != null) {
+                bigContentView.setTextColor(com.android.internal.R.id.title, mHeadsUpTextColor);
+                bigContentView.setTextColor(com.android.internal.R.id.text, mHeadsUpTextColor);
+                bigContentView.setTextColor(com.android.internal.R.id.big_text, mHeadsUpTextColor);
+                bigContentView.setTextColor(com.android.internal.R.id.time, mHeadsUpTextColor);
+//                bigContentView.setTextColor(com.android.internal.R.id.action0, mHeadsUpTextColor);
+                bigContentView.setTextColor(com.android.internal.R.id.text2, mHeadsUpTextColor);
+                bigContentView.setTextColor(com.android.internal.R.id.info, mHeadsUpTextColor);
+            }
         }
 
         // create the row view
@@ -1025,7 +1048,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
         // Construct the expanded view.
         NotificationData.Entry entry = new NotificationData.Entry(key, notification, iconView);
-        if (!inflateViews(entry, mPile)) {
+        if (!inflateViews(entry, mPile, 0)) {
             handleNotificationError(key, notification, "Couldn't expand RemoteViews for: "
                     + notification);
             return null;
