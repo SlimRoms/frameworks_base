478b07a6c1173fbba3d98516d4c679723166c73a
Jean Trivelly <jean.trivelly@intel.com>
2014-10-02 10:41:18 +0200

    Do not filterout wifidisplay we are connecting to
    During Widi connection we may still received scan results on
    p2p interface. If the scan result from the display we are currently
    connecting to does not set the bit SessionAvaible in its wfd info
    field - which is OK since it has an ongoing connection -, it will
    be filterout from the scan result forwarded to upper layer. This
    will cause upper layer (MediaRouter) to cancel the on-going connection
    assuming the display is no longer available.
    
    This patch does not filterout the display we are currently
    connecting from scan result forwarded by WifiDisplayController.
    
    Change-Id: I682a148351cbefdce2b55590c66ac12027c50ba5
    Signed-off-by: Cedric IZOARD <cedricx.izoard@intel.com>
    Signed-off-by: Jean Trivelly <jean.trivelly@intel.com>
    Signed-off-by: Aymen Ben Alaya <aymenx.ben.alaya@intel.com>
    Signed-off-by: Abdelmajid MLAYEH <abdelmajidx.mlayeh@intel.com>

--- services/core/java/com/android/server/display/WifiDisplayController.java
+++ services/core/java/com/android/server/display/WifiDisplayController.java
@@ -448,7 +448,7 @@ final class WifiDisplayController implements DumpUtils.Dump {
                         Slog.d(TAG, "  " + describeWifiP2pDevice(device));
                     }
 
-                    if (isWifiDisplay(device)) {
+                    if (isWifiDisplay(device) || device.equals(mConnectingDevice)) {
                         mAvailableWifiDisplayPeers.add(device);
                     }
                 }
