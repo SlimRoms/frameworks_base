f5edaf9facf4be09bb3067cb093bcbd10ca36eaf
Clark Scheff <clark@cyngn.com>
2014-11-14 09:44:51 -0800

    Add clear all recents action to recents panel
    Change-Id: I2243ebc83abc483e78b112900a653d7e61edc5ba

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -40,6 +40,8 @@ import com.android.systemui.recents.model.RecentsTaskLoader;
 import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
 
+import com.android.systemui.R;
+
 import java.util.ArrayList;
 
 /**
@@ -157,7 +159,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             View child = getChildAt(i);
             if (child != mSearchBar) {
                 TaskStackView stackView = (TaskStackView) child;
-                //stackView.dismissAllTasks();
+                stackView.dismissAllTasks();
             }
         }
     }
@@ -326,12 +328,12 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     @Override
     protected void onAttachedToWindow () {
         super.onAttachedToWindow();
-        //mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
-        //mClearRecents.setOnClickListener(new View.OnClickListener() {
-        //    public void onClick(View v) {
-        //        dismissAllTasksAnimated();
-        ///    }
-       // });
+        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
+        mClearRecents.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dismissAllTasksAnimated();
+            }
+        });
     }
 
     /**
