c48666102160de8ff40943900fc97b21352a5b3d
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-11-17 13:56:05 +0200

    frameworks/base: Bringup expanded desktop and implement per-user configuration.
    ps: formatting
    
    Change-Id: I27b2a4174f544f3437a5c918581ae599e4c3369b

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -427,6 +427,8 @@ public interface WindowManagerPolicy {
 
         /** Unregister a system listener for touch events */
         void unregisterPointerEventListener(PointerEventListener listener);
+
+        void addSystemUIVisibilityFlag(int flags);
     }
 
     public interface PointerEventListener {
