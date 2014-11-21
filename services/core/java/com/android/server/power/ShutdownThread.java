8a53cdaa1fad179d411a71b56c7ca2008bf98b65
Lars Greiss <kufikugel@googlemail.com>
2014-11-21 22:40:46 +0100

    Frameworks: Add reboot menu - prepare for Slims Global menu - misc changes
    Adding advanced reboot menu like it was on kk4.4
    Adding advanced reboot secure option
    Move reboot into safe mode from longpress power to longpress reboot
    Enable for now per default Airplane mode and sound and reboot till we have imported our SlimActions class to make in configurable
    
    misc changes and adaptions to lp
    
    Change-Id: Idd4f56d985ac51ade5f8deb2597d8f1dda22088a

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -22,6 +22,7 @@ import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.IActivityManager;
+import android.app.KeyguardManager;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.IBluetoothManager;
@@ -60,6 +61,7 @@ import com.android.server.power.PowerManagerService;
 
 import android.util.Log;
 import android.view.IWindowManager;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 
 import java.io.BufferedReader;
@@ -70,6 +72,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+
+import org.slim.provider.SlimSettings;
+import org.slim.utils.BuildInfo;
 
 public final class ShutdownThread extends Thread {
     // constants
@@ -188,27 +193,79 @@ public final class ShutdownThread extends Thread {
             resourceId = com.android.internal.R.string.reboot_confirm;
         }
 
+        final int titleResourceId = mRebootSafeMode
+                 ? com.android.internal.R.string.reboot_safemode_title
+                 : (mReboot
+                         ? com.android.internal.R.string.reboot_system
+                         : com.android.internal.R.string.power_off);
+
         Log.d(TAG, "Notifying thread to start shutdown longPressBehavior=" + longPressBehavior);
 
         if (confirm) {
             final CloseDialogReceiver closer = new CloseDialogReceiver(context);
             if (sConfirmDialog != null) {
                 sConfirmDialog.dismiss();
+                sConfirmDialog = null;
             }
-            sConfirmDialog = new AlertDialog.Builder(context)
-                    .setTitle(mRebootSafeMode
-                            ? com.android.internal.R.string.reboot_safemode_title
-                            : showRebootOption
-                                    ? com.android.internal.R.string.reboot_title
-                                    : com.android.internal.R.string.power_off)
-                    .setMessage(resourceId)
-                    .setPositiveButton(com.android.internal.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            beginShutdownSequence(context);
-                        }
-                    })
-                    .setNegativeButton(com.android.internal.R.string.no, null)
-                    .create();
+            if (mReboot && !mRebootSafeMode) {
+                // Determine if primary user is logged in
+                boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
+
+                // See if the advanced reboot menu is enabled
+                // (only if primary user) and check the keyguard state
+                int advancedReboot = isPrimary ? getAdvancedReboot(context) : 0;
+                KeyguardManager km = (KeyguardManager) context.getSystemService(
+                        Context.KEYGUARD_SERVICE);
+                boolean locked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
+
+                if ((advancedReboot == 1 && !locked) || advancedReboot == 2) {
+                    // Include options in power menu for rebooting into recovery or bootloader
+                    sConfirmDialog = new AlertDialog.Builder(context)
+                            .setTitle(titleResourceId)
+                            .setItems(
+                                    com.android.internal.R.array.shutdown_reboot_options,
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (which < 0)
+                                        return;
+
+                                    String actions[] = context.getResources().getStringArray(
+                                            com.android.internal.R.array.shutdown_reboot_actions);
+
+                                    if (actions != null && which < actions.length)
+                                        mReason = actions[which];
+
+                                    mReboot = true;
+                                    beginShutdownSequence(context);
+                                }
+                            })
+                            .create();
+                            sConfirmDialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+                                public boolean onKey (DialogInterface dialog, int keyCode,
+                                        KeyEvent event) {
+                                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                                        mReboot = false;
+                                        dialog.cancel();
+                                    }
+                                    return true;
+                                }
+                            });
+                }
+            }
+
+            if (sConfirmDialog == null) {
+                sConfirmDialog = new AlertDialog.Builder(context)
+                        .setTitle(titleResourceId)
+                        .setMessage(resourceId)
+                        .setPositiveButton(com.android.internal.R.string.yes,
+                                new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                beginShutdownSequence(context);
+                            }
+                        })
+                        .setNegativeButton(com.android.internal.R.string.no, null)
+                        .create();
+            }
             closer.dialog = sConfirmDialog;
             sConfirmDialog.setOnDismissListener(closer);
             sConfirmDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
@@ -216,6 +273,11 @@ public final class ShutdownThread extends Thread {
         } else {
             beginShutdownSequence(context);
         }
+    }
+
+    private static int getAdvancedReboot(Context context) {
+        return SlimSettings.Secure.getInt(context.getContentResolver(),
+                SlimSettings.Secure.ADVANCED_REBOOT, 0);
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
@@ -352,10 +414,13 @@ public final class ShutdownThread extends Thread {
             }
         } else if (PowerManager.REBOOT_RECOVERY.equals(mReason)) {
             // Factory reset path. Set the dialog message accordingly.
-            pd.setTitle(context.getText(com.android.internal.R.string.reboot_to_reset_title));
+            pd.setTitle(context.getText(com.android.internal.R.string.reboot_title));
             pd.setMessage(context.getText(
-                        com.android.internal.R.string.reboot_to_reset_message));
+                        com.android.internal.R.string.reboot_progress));
             pd.setIndeterminate(true);
+        } else if (mReboot) {
+            pd.setTitle(context.getText(com.android.internal.R.string.reboot_title));
+            pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
         } else {
             pd.setTitle(context.getText(com.android.internal.R.string.power_off));
             pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
