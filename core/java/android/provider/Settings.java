6e66a3f2c56662cdea18a8480d270b0e6bc78026
Muhammed Nazim <mnm9994u@gmail.com>
2014-11-29 10:28:06 +0100

    Forward Port: Hide USB Debugging Icon [1/2]
    Change-Id: Ibd63a9df207ddfb67dc16fbd44697261a9f6f0b4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3348,6 +3348,7 @@ public final class Settings {
 
             MOVED_TO_GLOBAL = new HashSet<String>();
             MOVED_TO_GLOBAL.add(Settings.Global.ADB_ENABLED);
+            MOVED_TO_GLOBAL.add(Settings.Global.ADB_NOTIFY);
             MOVED_TO_GLOBAL.add(Settings.Global.ASSISTED_GPS_ENABLED);
             MOVED_TO_GLOBAL.add(Settings.Global.BLUETOOTH_ON);
             MOVED_TO_GLOBAL.add(Settings.Global.BUGREPORT_IN_POWER_MENU);
@@ -3841,6 +3842,14 @@ public final class Settings {
          */
         @Deprecated
         public static final String ADB_ENABLED = Global.ADB_ENABLED;
+
+       /**
+        * Whether to display the ADB notification.
+        * @deprecated Use {@link android.provider.Settings.Global#ADB_NOTIFY} instead
+        * @hide
+        */
+        @Deprecated
+        public static final String ADB_NOTIFY = "adb_notify";
 
         /**
          * The hostname for this device
@@ -5641,6 +5650,12 @@ public final class Settings {
          * Whether ADB is enabled.
          */
         public static final String ADB_ENABLED = "adb_enabled";
+
+       /**
+        * Whether to display the ADB notification.
+        * @hide
+        */
+        public static final String ADB_NOTIFY = "adb_notify";
 
         /**
          * Whether Views are allowed to save their attribute data.
