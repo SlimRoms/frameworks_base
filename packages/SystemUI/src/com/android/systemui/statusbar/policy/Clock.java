43952b2537d64ec75278e329e5c0edea2575837e
Lars Greiss <kufikugel@googlemail.com>
2014-12-05 00:26:01 +0100

    SystemUI: Status bar clock - fixes
    This commit fixes:
    
    - no overlapping more from status bar icons on center clock.
      The width depending on what the user has setuped (date and clock) is exactly meassured now
    - do not start animations if they are not needed
    - reduce settings provider calls
    - a bit cleanup
    
    Change-Id: I7a37a6ae8f9bad0f265119af05dc20cf21cd6890

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -39,6 +39,7 @@ import android.widget.TextView;
 
 import com.android.systemui.DemoMode;
 import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.PhoneStatusBar;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -80,10 +81,12 @@ public class Clock extends TextView implements DemoMode {
     protected int mClockDateStyle = CLOCK_DATE_STYLE_REGULAR;
     protected int mClockStyle = STYLE_CLOCK_RIGHT;
     protected boolean mShowClock;
+    private int mClockAndDateWidth;
 
     private int mAmPmStyle;
 
     private SettingsObserver mSettingsObserver;
+    private PhoneStatusBar mStatusBar;
 
     protected class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -361,6 +364,10 @@ public class Clock extends TextView implements DemoMode {
             updateClock();
         }
 
+        if (mStatusBar != null) {
+            mStatusBar.setClockAndDateStatus(mClockAndDateWidth, mClockStyle, mShowClock);
+        }
+
     }
 
     protected void updateClockVisibility() {
@@ -394,4 +401,18 @@ public class Clock extends TextView implements DemoMode {
             setText(getSmallTime());
         }
     }
+
+    public void setPhoneStatusBar(PhoneStatusBar statusBar) {
+        mStatusBar = statusBar;
+    }
+
+    @Override
+    protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
+        super.onSizeChanged(xNew, yNew, xOld, yOld);
+        mClockAndDateWidth = xNew;
+        if (mStatusBar != null) {
+            mStatusBar.setClockAndDateStatus(mClockAndDateWidth, mClockStyle, mShowClock);
+        }
+    }
+
 }
