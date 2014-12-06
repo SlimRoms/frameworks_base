fb35797560d7d54533fba7f054989e75747069f4
Lars Greiss <kufikugel@googlemail.com>
2014-12-06 17:23:35 +0100

    Keyguard: Enable quick unlock (1/2)
    old way does not work anymore exactly like before. So adapt the idea to new LP code
    
    Thankd DVTonder for the basic idea prior LP
    
    PS:
    - do PIN view correctly
    
    Change-Id: I86660afafb8648d36d6c135343c88afcfc5173fc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5228,6 +5228,12 @@ public final class Settings {
                 = "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
+
+        /**
          * Whether to include options in power menu for rebooting into recovery and bootloader
          * @hide
          */
@@ -5382,7 +5388,8 @@ public final class Settings {
             PRIVACY_GUARD_DEFAULT,
             PRIVACY_GUARD_NOTIFICATION,
             ADVANCED_REBOOT,
-            STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD
+            STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
+            LOCKSCREEN_QUICK_UNLOCK_CONTROL
         };
 
         /**
