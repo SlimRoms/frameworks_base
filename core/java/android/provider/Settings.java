37d24d014b9023681be1064f66b72a47741c7994
Lars Greiss <kufikugel@googlemail.com>
2014-12-08 23:25:36 +0100

    Frameworks: Keyguard - Add user configurable shortcuts
    Change-Id: I18707586d9edcf1d756f50ecab3a555f85cea799

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2035,6 +2035,21 @@ public final class Settings {
          */
         public static final int SCREEN_BRIGHTNESS_MODE_AUTOMATIC = 1;
 
+        /**
+         * Defines the shortcuts to be shown on lockscreen
+         * Usage is like this: target:icon|target:icon|target:icon
+         * if :icon is not set, default application icon will be used
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+        /**
+         * Whether shorcuts open with normal or longpress
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS_LONGPRESS =
+                "lockscreen_shortcuts_longpress";
+
         /** 
          * Whether to allow one finger quick settings expansion on the right side of the statusbar.
          *
