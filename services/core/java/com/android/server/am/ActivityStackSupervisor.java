ba8578b9bbff012af71f0efe4d34e290bc115bf1
Varad Deshmukh <varadd@codeaurora.org>
2014-12-17 15:35:26 -0800

    Delay Explicit GC from the SystemServer.
    Binder activity as part of the System Server executes an Explicit
    GC every 200 Binder references. Such Binder activity is significant
    during the application launch of some Google Apps like Music.
    The resulting pause due to the GC pauses the main application, affecting
    launch time.
    
    This change detects an app launch, and delays the incoming GC by
    3 seconds. The API for GC delay can be calllable for different use-cases.
    This change deals with only the application launch.
    
    CRs-Fixed: 779174
    
    Change-Id: I0f2dfaf3cb13940c77df8a6fefc2cbc0c76e027c

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2723,6 +2723,10 @@ public final class ActivityStackSupervisor implements DisplayListener {
         BinderInternal.modifyDelayedGcParams();
 
         mPm.cpuBoost(2000 * 1000);
+
+        /* Delay Binder Explicit GC during application launch */
+        BinderInternal.modifyDelayedGcParams();
+
         if (DEBUG_TASKS) Slog.d(TAG, "No task found");
         return null;
     }
