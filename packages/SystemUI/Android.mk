2776acfc7be96d0419096ee84ab7b14994b94bb7
dankoman <dankoman30@gmail.com>
2014-12-23 17:57:21 +0100

    [2/3] Frameworks: Slim doze options
    Conflicts:
    	packages/SystemUI/res/values/yaosp_config.xml
    
    Change-Id: I2a1be925ac58721f12da04a6164641ffa1c4a654
    
    add some dependencies
    
    Change-Id: I3791fe88704d576ffa3cc3bdf00d8498f25cb8fc

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -7,7 +7,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
     src/com/android/systemui/EventLogTags.logtags
 
 LOCAL_STATIC_JAVA_LIBRARIES := Keyguard
-LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_JAVA_LIBRARIES := telephony-common \
+    org.slim.framework
 
 LOCAL_PACKAGE_NAME := SystemUI
 LOCAL_CERTIFICATE := platform
