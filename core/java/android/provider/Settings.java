1849bf14495661b39440c72f37b9c9102633a104
yashade2001 <yashade2001@gmail.com>
2014-12-26 10:09:16 +0100

    Recents: option to disable searchbar in stock recents [2/2]
    Change-Id: I6366d1bb9b586ceecb2f83a64ae4db24e6a383b5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3567,6 +3567,12 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_NETWORK_ACTIVITY = "status_bar_show_network_activity";
 
         /**
+         * Recents Show/Hide Search Bar
+         * @hide
+         */
+        public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
