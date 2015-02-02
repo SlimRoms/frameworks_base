fcc975b456952a1673a39abc7d30d5f1b8c1b2b2
Danny Baumann <dannybaumann@web.de>
2015-02-02 17:31:38 +0100

    Remove pointless notification drawer network info label in MSIM mode.
    Change-Id: I44761c152094ae71050cbb7faf9ee1671b6c2dc4

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
@@ -114,7 +114,6 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
     int PHONE_ID2 = PhoneConstants.PHONE_ID2;
     private HashMap<Integer, Integer> mSubIdPhoneIdMap;
     ArrayList<MSimSignalCluster> mSimSignalClusters = new ArrayList<MSimSignalCluster>();
-    ArrayList<TextView> mSubsLabelViews = new ArrayList<TextView>();
 
     // Whether the direction arrows are enabled by the user
     boolean mDirectionArrowsEnabled = false;
@@ -485,10 +484,6 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
         }
     }
 
-    public void addSubsLabelView(TextView v) {
-        mSubsLabelViews.add(v);
-    }
-
     private void updateCarrierText(int sub) {
         int textResId = 0;
         if (mAirplaneMode) {
@@ -538,17 +533,10 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
             carrierName = carrierName + "    " + mCarrierTextSub[i];
         }
 
-        if (mContext.getResources().getBoolean(R.bool.config_showDataConnectionView)) {
-            for (int i = 0; i < mSubsLabelViews.size(); i++) {
-                TextView v = mSubsLabelViews.get(i);
-                v.setText(carrierName);
-            }
-        } else {
-            for (int i = 0; i < mMobileLabelViews.size(); i++) {
-                TextView v = mMobileLabelViews.get(i);
-                v.setText(carrierName);
-                v.setVisibility(View.VISIBLE);
-            }
+        for (int i = 0; i < mMobileLabelViews.size(); i++) {
+            TextView v = mMobileLabelViews.get(i);
+            v.setText(carrierName);
+            v.setVisibility(View.VISIBLE);
         }
     }
 
