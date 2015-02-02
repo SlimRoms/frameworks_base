998cd1dbe2fa2170d40d265e667f707c898dfc43
Danny Baumann <dannybaumann@web.de>
2015-02-02 17:30:39 +0100

    Improve layout of MSIM SIM PIN/PUK keyguard views.
    Put the SIM name to the right of the SIM icon instead of under it, and
    add some reasonable padding.
    
    Change-Id: I7d466b28e1cb3ef27701d387ffd87cf9df408284

--- packages/Keyguard/src/com/android/keyguard/KeyguardPinBasedInputView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardPinBasedInputView.java
@@ -17,6 +17,7 @@
 package com.android.keyguard;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Rect;
 import android.provider.Settings;
 import android.os.UserHandle;
@@ -230,5 +231,20 @@ public abstract class KeyguardPinBasedInputView extends KeyguardAbsKeyInputView
             return true;
         }
         return false;
+    }
+
+    protected void applyPaddingToView(View view, int paddingDimenResId) {
+        final Rect backgroundPadding = new Rect();
+
+        if (view.getBackground() != null) {
+            view.getBackground().getPadding(backgroundPadding);
+        }
+
+        final Resources res = getContext().getResources();
+        final int defaultPadding = res.getDimensionPixelSize(paddingDimenResId);
+        view.setPadding(backgroundPadding.left + defaultPadding,
+                backgroundPadding.top + defaultPadding,
+                backgroundPadding.right + defaultPadding,
+                backgroundPadding.bottom + defaultPadding);
     }
 }
