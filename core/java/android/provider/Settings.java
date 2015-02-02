aa5ad8fb2133cd64763a2355a3771efc0f0a5eae
dankoman <dankoman30@gmail.com>
2015-02-02 01:37:07 +0100

    [2/2] Framework: Slim doze options
    option to turn off TriggerSensors for doze pulse (pick-up/
            significant motion sensors)
    
    patchset: doze schedule preference
    
    patchset: notification trigger preference
    
    patchset: split pickup gesture sensor and significant
            motion sensor triggers into two preferences
    
    patchset: doze brightness
    
    patchset: custom values to slim_config
    Change-Id: I2a1be925ac58721f12da04a6164641ffa1c4a654

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2105,6 +2105,36 @@ public final class Settings {
         public static final String DOZE_TIMEOUT = "doze_timeout";
 
         /**
+         * Use pick up gesture sensor as doze pulse trigger
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_PICKUP = "doze_trigger_pickup";
+
+        /**
+         * Use significant motion sensor as doze pulse trigger
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_SIGMOTION = "doze_trigger_sigmotion";
+
+        /**
+         * Use notifications as doze pulse triggers
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_NOTIFICATION = "doze_trigger_notification";
+
+        /**
+         * Follow pre-configured doze pulse repeat schedule
+         * @hide
+         */
+        public static final String DOZE_SCHEDULE = "doze_schedule";
+
+        /**
+         * Doze pulse screen brightness level
+         * @hide
+         */
+        public static final String DOZE_BRIGHTNESS = "doze_brightness";
+
+        /**
          * Defines the status and navigation bar color is battery safe mode is enabled.
          *
          * @hide
