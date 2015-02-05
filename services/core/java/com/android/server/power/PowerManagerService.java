091168b357b956e4907a8e55b657fcb0b4b6a3bd
dankoman <dankoman30@gmail.com>
2015-02-05 15:12:13 +0100

    [1/2] Frameworks: let InCallUI handle proximity sensor for incoming calls
    fixes "proximity sensor doesn't control screen state on incoming
            call.  must manually turn on screen to see who's calling."
    
    Change-Id: I738b3bbd328992c8a8010a0c8d9aa1e444cfa9f9

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -66,6 +66,7 @@ import android.os.UserHandle;
 import android.os.WorkSource;
 import android.provider.Settings;
 import android.service.dreams.DreamManagerInternal;
+import android.telephony.TelephonyManager;
 import android.util.EventLog;
 import android.util.Slog;
 import android.util.TimeUtils;
@@ -183,6 +184,7 @@ public final class PowerManagerService extends SystemService
     private DreamManagerInternal mDreamManager;
     private Light mAttentionLight;
     private Light mButtonsLight;
+    private TelephonyManager mTelephonyManager;
 
     private final Object mLock = new Object();
 
@@ -3146,7 +3148,12 @@ public final class PowerManagerService extends SystemService
                 // There is already a message queued;
                 return;
             }
-            if (mProximityWakeSupported && mProximityWakeEnabled && mProximitySensor != null) {
+
+            boolean hasIncomingCall = (getTelephonyManager().getCallState()
+                    == TelephonyManager.CALL_STATE_RINGING);
+
+            if (mProximityWakeSupported && mProximityWakeEnabled && mProximitySensor != null
+                    && !hasIncomingCall) {
                 Message msg = mHandler.obtainMessage(MSG_WAKE_UP);
                 msg.obj = r;
                 mHandler.sendMessageDelayed(msg, mProximityTimeOut);
@@ -3154,6 +3161,14 @@ public final class PowerManagerService extends SystemService
             } else {
                 r.run();
             }
+        }
+
+        TelephonyManager getTelephonyManager() {
+            if (mTelephonyManager == null) {
+                mTelephonyManager = (TelephonyManager)mContext.getSystemService(
+                        Context.TELEPHONY_SERVICE);
+            }
+            return mTelephonyManager;
         }
 
         private void runPostProximityCheck(final Runnable r) {
