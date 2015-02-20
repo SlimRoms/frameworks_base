1423419dcdf2be5f19611b0d13c173e64659022c
Amr BEN ABDESSALEM <amrx.ben.abdessalem@intel.com>
2015-02-20 12:10:28 +0100

    Widi disconnection should be done when powering off
    This patch disconnect the DUT from Widi dongle when we are powering off,
    to avoid displaying power off screen more than 10 secs on external screen
    even the device is powered off.
    
    Change-Id: I58357dd013fe0eabeddb4567bf7dfd9afab4ea7d
    Signed-off-by: Amr BEN ABDESSALEM <amrx.ben.abdessalem@intel.com>
    Signed-off-by: Matthieu MAUGER <matthieux.mauger@intel.com>
    Signed-off-by: Abdelmajid MLAYEH <abdelmajidx.mlayeh@intel.com>

--- services/core/java/com/android/server/display/WifiDisplayController.java
+++ services/core/java/com/android/server/display/WifiDisplayController.java
@@ -177,6 +177,7 @@ final class WifiDisplayController implements DumpUtils.Dump {
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        intentFilter.addAction(Intent.ACTION_SHUTDOWN);
         context.registerReceiver(mWifiP2pReceiver, intentFilter, null, mHandler);
 
         ContentObserver settingsObserver = new ContentObserver(mHandler) {
@@ -1108,6 +1109,10 @@ final class WifiDisplayController implements DumpUtils.Dump {
                 if (DEBUG) {
                     Slog.d(TAG, "Received WIFI_P2P_THIS_DEVICE_CHANGED_ACTION: mThisDevice= "
                             + mThisDevice);
+                }
+            } else if (action.equals(Intent.ACTION_SHUTDOWN)) {
+                if (mConnectedDevice != null) {
+                    disconnect();
                 }
             }
         }
