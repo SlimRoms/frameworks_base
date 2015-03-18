79bbd6377caf82fd01a9e3a2e2dd0471de2f03d5
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-03-19 00:52:48 +0100

    [2/2] frameworks_base: Add options for battery icon and text in status bar
    Available options :
    
    - icon : portrait, landscape battery / normal, dotted circle / text only / none
    - battery percent : none / inside icon / next to icon
    
    Charging with portrait and landscape icons :
    
    - without battery percent inside --> show a bolt inside of icon blinking
    - with battery percent inside --> alternate every second between battery percent and bolt inside of icon
    
    Charging with circle icon :
    
    - without battery percent inside --> charged subset of circle will rotate with bolt inside blinking
    - with battery percent inside --> charged subset of circle will rotate with bolt and battery percent alternating inside
    
    Charging text only :
    
    - will show 85~ instead of 85% when charging
    
    To do for v1 :
    
    - nothing, can be merged from my point of view
    
    To do for v2 :
    
    - animate battery percent for text only style
    - rewrite battery drawing code to properly partially invert text/bolt
    
    Based on Zavon Joseph patchset 5
    
    Extended with text option by Jean-Pierre Rasquin <yank555.lu@gmail.com> :
    
    - fixed text not showing on lockscreen at all
    
    - fixed always displaying text when charging on lockscreen bug, now respects user setting on lockscreen as well
    
    - fixed text not showing on expanded notifications
    
    - fixed text only won't stick, will show portrait battery plus text on reboot, settings will reflect last user setting, and be different to what is actually applied
    
    - fixed text will always show on expanded notifications, even when quick tiles are not showing
    
    - fixed white on white landscape in icon battery text (text will switch color as soon as 25% is reached)
    
    - added only show text next to icon in expanded notifications when percent is not shown in the icon, else it's redundant
    
    - fixed code layout
    
    - added charging animation for portrait / landscape icon when battery percent is set to 'inside icon'
    
    - fixed charging bolt not correctly showing on landscape icon
    
    - added charging animation (battery percent - bolt) to circle battery icon
    
    - fixed my derp that never showed charge bolt when battery percent was set to hidden or next to icon
    
    - added animate bolt even when battery percent set to hidden
    
    - fixed charged part of battery icon not drawn between hardcoded critical level (15) and battery safe mode level
    
    - fixed battery percent not shown when not opaque (low battery, white color)
    
    - maxed out circle battery icon size (thx to Griffin Millender)
    
    - added dotted battery icon style (thx to Griffin Millender)
    
    - removed two (make that three) unnecessary observers (thx to Griffin Millender)
    
    - fixed the missing update to notification battery icon (thx to Griffin Millender)
    
    - tiny code cleanup in BatteryMeterView.java
    
    - removed BatteryLevelTextView class, this is not needed
    
    - removed one more unnecessary observer (in BatteryLevelTextView)
    
    - added updating battery level text from PhoneStatusBar to StatusBarHeaderView and KeyguardStatusBarView  to avoid the user actually seeing the update when pulling down notifications and battery has dropped since last time notifications were pulled and getting to lockscreen (unlikely, but this way it's settled for good)
    
    - added battery style text only charging feedback (preliminary, will show 85~ instead of 85%)
    
    Change-Id: I6b6eff25759740b9288c54522e0b7909ea51db8c
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -18,6 +18,9 @@ package com.android.systemui.statusbar.phone;
 
 import android.animation.LayoutTransition;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -29,7 +32,6 @@ import android.view.animation.Interpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.BatteryController;
@@ -44,7 +46,6 @@ import java.text.NumberFormat;
 public class KeyguardStatusBarView extends RelativeLayout
         implements BatteryController.BatteryStateChangeCallback {
 
-    private boolean mBatteryCharging;
     private boolean mKeyguardUserSwitcherShowing;
     private boolean mBatteryListening;
 
@@ -52,6 +53,7 @@ public class KeyguardStatusBarView extends RelativeLayout
     private View mSystemIconsSuperContainer;
     private MultiUserSwitch mMultiUserSwitch;
     private ImageView mMultiUserAvatar;
+    private BatteryMeterView mBatteryView;
     private TextView mBatteryLevel;
 
     private BatteryController mBatteryController;
@@ -60,8 +62,38 @@ public class KeyguardStatusBarView extends RelativeLayout
     private int mSystemIconsSwitcherHiddenExpandedMargin;
     private Interpolator mFastOutSlowInInterpolator;
 
+    private boolean mShowBatteryText;
+    private boolean mShowBatteryTextCharging;
+    private boolean mBatteryIsCharging;
+    private int mBatteryChargeLevel;
+
     public KeyguardStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        loadShowBatteryTextSetting();
+    }
+
+    private void loadShowBatteryTextSetting() {
+        mShowBatteryText = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0) == 2;
+        int batteryStyle = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        switch (batteryStyle) {
+            case 4:
+                //meterMode = BatteryMeterMode.BATTERY_METER_GONE;
+                mShowBatteryText = false;
+                mShowBatteryTextCharging = false;
+                break;
+
+            case 6:
+                //meterMode = BatteryMeterMode.BATTERY_METER_TEXT;
+                mShowBatteryText = true;
+                mShowBatteryTextCharging = true;
+                break;
+
+            default:
+                mShowBatteryTextCharging = false;
+                break;
+        }
     }
 
     @Override
@@ -70,12 +102,14 @@ public class KeyguardStatusBarView extends RelativeLayout
         mSystemIconsSuperContainer = findViewById(R.id.system_icons_super_container);
         mMultiUserSwitch = (MultiUserSwitch) findViewById(R.id.multi_user_switch);
         mMultiUserAvatar = (ImageView) findViewById(R.id.multi_user_avatar);
-        mBatteryLevel = (TextView) findViewById(R.id.battery_level);
+        mBatteryView = (BatteryMeterView) findViewById(R.id.battery);
+        mBatteryLevel = (TextView) findViewById(R.id.battery_level_text);
         mCarrierLabel = (TextView) findViewById(R.id.keyguard_carrier_text);
         loadDimens();
         mFastOutSlowInInterpolator = AnimationUtils.loadInterpolator(getContext(),
                 android.R.interpolator.fast_out_slow_in);
         updateUserSwitcher();
+        updateVisibilities();
     }
 
     @Override
@@ -104,7 +138,9 @@ public class KeyguardStatusBarView extends RelativeLayout
         } else if (mMultiUserSwitch.getParent() == this && mKeyguardUserSwitcherShowing) {
             removeView(mMultiUserSwitch);
         }
-        mBatteryLevel.setVisibility(mBatteryCharging ? View.VISIBLE : View.GONE);
+        loadShowBatteryTextSetting();
+        updateBatteryLevelText();
+        mBatteryLevel.setVisibility(mShowBatteryText ? View.VISIBLE : View.GONE);
     }
 
     private void updateSystemIconsLayoutParams() {
@@ -136,9 +172,14 @@ public class KeyguardStatusBarView extends RelativeLayout
         mMultiUserSwitch.setKeyguardMode(keyguardSwitcherAvailable);
     }
 
+    public void updateBatteryIconSettings() {
+        mBatteryView.updateBatteryIconSettings();
+        updateVisibilities();
+    };
+
     public void setBatteryController(BatteryController batteryController) {
         mBatteryController = batteryController;
-        ((BatteryMeterView) findViewById(R.id.battery)).setBatteryController(batteryController);
+        mBatteryView.setBatteryController(batteryController);
     }
 
     public void setUserInfoController(UserInfoController userInfoController) {
@@ -150,15 +191,26 @@ public class KeyguardStatusBarView extends RelativeLayout
         });
     }
 
+    private void updateBatteryLevelText() {
+        if (mBatteryIsCharging & mShowBatteryTextCharging) {
+            mBatteryLevel.setText(getResources().getString(
+                    R.string.battery_level_template_charging, mBatteryChargeLevel));
+        } else {
+            mBatteryLevel.setText(getResources().getString(
+                    R.string.battery_level_template, mBatteryChargeLevel));
+        }
+    }
+
+    public void updateBatteryLevel(int level, boolean charging) {
+        mBatteryIsCharging = charging;
+        mBatteryChargeLevel = level;
+        loadShowBatteryTextSetting();
+        updateBatteryLevelText();
+    }
+
     @Override
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
-        String percentage = NumberFormat.getPercentInstance().format((double) level / 100.0);
-        mBatteryLevel.setText(percentage);
-        boolean changed = mBatteryCharging != charging;
-        mBatteryCharging = charging;
-        if (changed) {
-            updateVisibilities();
-        }
+        updateBatteryLevel(level, charging);
     }
 
     @Override
@@ -240,5 +292,14 @@ public class KeyguardStatusBarView extends RelativeLayout
     @Override
     public boolean hasOverlappingRendering() {
         return false;
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+        if (mBatteryController != null) {
+            mBatteryController.removeStateChangedCallback(this);
+        }
     }
 }
