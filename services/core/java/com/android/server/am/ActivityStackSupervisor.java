1074126c79cce14eef6a09ca882830557f5684e3
Steve Kondik <steve@cyngn.com>
2015-03-27 23:24:37 -0700

    Revert QC performance manager, we have our own.
    This reverts commit e9bfde26b69a0346a3d280a2edcbfddc4f0d9d38.
    
    Change-Id: I542bd6cc61b3c0f3c9b665d506a9d966c3344d34

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -84,6 +84,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.Trace;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
@@ -115,10 +116,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-
-/* Perf */
-import org.codeaurora.Performance;
-import android.os.Trace;
 
 public final class ActivityStackSupervisor implements DisplayListener {
     static final boolean DEBUG = ActivityManagerService.DEBUG || false;
