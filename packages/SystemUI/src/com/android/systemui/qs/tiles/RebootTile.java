aef529d2f76b6ea1c3074d71c4bd76cba3c9ec71
Griffin Millender <griffinn.millender@gmail.com>
2015-05-08 00:40:34 -0500

    Revamp reboot tile
    Change-Id: I7d95db15db6478d00a874e487322010a642b0628

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -17,17 +17,37 @@
 package com.android.systemui.qs.tiles;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import android.util.Log;
+
 import com.android.systemui.R;
+import com.android.systemui.qs.QSDetailItems.Item;
+import com.android.systemui.qs.QSDetailItemsList;
 import com.android.systemui.qs.QSTile;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RebootTile extends QSTile<QSTile.BooleanState> {
 
-    private boolean mRebootToRecovery = false;
+    private final RebootDetailAdapter mDetailAdapter;
 
     public RebootTile(Host host) {
         super(host);
+        mDetailAdapter = new RebootDetailAdapter();
+    }
+
+    @Override
+    public DetailAdapter getDetailAdapter() {
+        return mDetailAdapter;
     }
 
     @Override
@@ -37,37 +57,106 @@ public class RebootTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     protected void handleClick() {
-        mRebootToRecovery = !mRebootToRecovery;
-        refreshState();
-    }
-
-    @Override
-    protected void handleLongClick() {
-        mHost.collapsePanels();
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                PowerManager pm =
-                    (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                pm.reboot(mRebootToRecovery ? "recovery" : "");
-            }
-        }, 500);
+        showDetail(true);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         state.visible = true;
-        if (mRebootToRecovery) {
-            state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
-        } else {
-            state.label = mContext.getString(R.string.quick_settings_reboot_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
-        }
+        state.label = mContext.getString(R.string.quick_settings_reboot_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
     }
 
     @Override
     public void setListening(boolean listening) {
+    }
+
+    private class RebootDetailAdapter implements DetailAdapter, AdapterView.OnItemClickListener {
+
+        private QSDetailItemsList mDetails;
+        private QSDetailItemsList.QSDetailListAdapter mAdapter;
+
+        private final List<Item> mRebootList = new ArrayList<>();
+
+        private int clickedPosition = -1;
+
+        @Override
+        public int getTitle() {
+            return R.string.quick_settings_reboot_label;
+        }
+
+        @Override
+        public void setToggleState(boolean state) {
+        }
+
+        @Override
+        public View createDetailView(Context context, View convertView, ViewGroup parent) {
+
+            rebuildRebootList(true);
+
+            mDetails = QSDetailItemsList.convertOrInflate(context, convertView, parent);
+            ListView list = mDetails.getListView();
+            list.setOnItemClickListener(this);
+            list.setAdapter(mAdapter =
+                    new QSDetailItemsList.QSDetailListAdapter(context, mRebootList));
+            mDetails.setEmptyState(R.drawable.ic_qs_reboot, getTitle());
+
+            return mDetails;
+        }
+
+        @Override
+        public Intent getSettingsIntent() {
+            return null;
+        }
+
+        @Override
+        public Boolean getToggleState() {
+            return null;
+        }
+
+        private void rebuildRebootList(boolean populate) {
+            mRebootList.clear();
+            String actions[] = mContext.getResources().getStringArray(
+                    com.android.internal.R.array.shutdown_reboot_actions);
+            String options[] = mContext.getResources().getStringArray(
+                    com.android.internal.R.array.shutdown_reboot_options);
+            if (populate) {
+                for (int i = 0; i < options.length; i++) {
+                    final Item item = new Item();
+                    item.tag = actions[i];
+                    item.line1 = options[i];
+                    mRebootList.add(item);
+                }
+            }
+            if (mAdapter != null) mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Item item = (Item) parent.getItemAtPosition(position);
+            if (item == null) return;
+            if (position != clickedPosition) {
+                for (int i = 0; i < mRebootList.size(); i++) {
+                    Item it = (Item) parent.getItemAtPosition(i);
+                    if (it != null) it.line2 = null;
+                }
+                clickedPosition = position;
+                item.line2 = mContext.getString(R.string.quick_settings_reboot_click_confirm);
+                mAdapter.notifyDataSetChanged();
+                return;
+            }
+            final String action = (String) item.tag;
+            clickedPosition = -1;
+            mHost.collapsePanels();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    PowerManager pm =
+                        (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+                    pm.reboot(action);
+                }
+            }, 500);
+        }
     }
 
 }
