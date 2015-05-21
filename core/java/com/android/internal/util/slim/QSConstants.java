982e202a02fe9c1891006b69945d6ae7990d10bb
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-05-21 21:31:03 +0200

    QS: Expanded desktop tile [1/2]
    PS2-3: fixed missing import and empty icon in the tile
    
    patchset 5: licensing
    Change-Id: I4043b8d90d844fb6d16d8477baa605c025f91473

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -49,6 +49,7 @@ public class QSConstants {
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
     public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_LIVE_DISPLAY = "live_display";
+    public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -88,5 +89,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
         TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
+        TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
     }
 }
