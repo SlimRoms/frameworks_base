50cd4c4e290dadfea73a3a6f1200d48635b9bc27
Victor Lapin <neonproject@gmail.com>
2015-05-25 23:27:45 +0300

    Heads up notifications behavior fix
    Heads up notifications are handled by "Show heads up" switch in the app 
    notification settings (except for core packages like SystemUI that have 
    heads up always on).
    Heads up for Hangouts can be disabled now.
    
    Change-Id: I823df8a93e10244bf5a8ce24672ea37fb05644f2

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2313,7 +2313,8 @@ public abstract class BaseStatusBar extends SystemUI implements
             return false;
         }
 
-        if (mHeadsUpNotificationView.isSnoozed(sbn.getPackageName())) {
+        String pkg = sbn.getPackageName();
+        if (mHeadsUpNotificationView.isSnoozed(pkg)) {
             return false;
         }
 
@@ -2329,9 +2330,15 @@ public abstract class BaseStatusBar extends SystemUI implements
                 || notification.vibrate != null;
         boolean isHighPriority = sbn.getScore() >= INTERRUPTION_THRESHOLD;
         boolean isFullscreen = notification.fullScreenIntent != null;
+        //denying heads up by default
         int asHeadsUp = notification.extras.getInt(Notification.EXTRA_AS_HEADS_UP,
-                Notification.HEADS_UP_ALLOWED);
-        boolean isAllowed = asHeadsUp != Notification.HEADS_UP_NEVER;
+                Notification.HEADS_UP_NEVER);
+        PackageManager pmUser = getPackageManagerForUser(
+                sbn.getUser().getIdentifier());
+        //but if it's a system package, heads up should still be shown
+        boolean isSystemPackage = isThisASystemPackage(pkg, pmUser);
+        boolean isAllowed = (asHeadsUp != Notification.HEADS_UP_NEVER) || isSystemPackage;
+        if (DEBUG) Log.d(TAG, "package: "+pkg+", isSystem: "+Boolean.toString(isSystemPackage)+", asHeadsUp: "+Integer.toString(asHeadsUp)+", isAllowed: "+Boolean.toString(isAllowed));
         boolean accessibilityForcesLaunch = isFullscreen
                 && mAccessibilityManager.isTouchExplorationEnabled();
 
@@ -2347,8 +2354,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && !keyguardIsShowing;
 
         if (!interrupt) {
-            boolean isHeadsUpPackage = mNoMan.getHeadsUpNotificationsEnabledForPackage(
-                    sbn.getPackageName(), sbn.getUid()) != Notification.HEADS_UP_NEVER;
+            boolean isHeadsUpPackage = (mNoMan.getHeadsUpNotificationsEnabledForPackage(
+                    pkg, sbn.getUid()) != Notification.HEADS_UP_NEVER) || isSystemPackage;
+            if (DEBUG) Log.d(TAG, "package: "+pkg+", isHeadsUpPackage: "+Boolean.toString(isHeadsUpPackage));
 
             boolean isExpanded = false;
             if (mStackScroller != null) {
@@ -2363,7 +2371,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                     && !keyguardIsShowing;
 
             if (interrupt) {
-                mHeadsUpPackageName = sbn.getPackageName();
+                mHeadsUpPackageName = pkg;
             }
         }
 
