c75655955e0e787a7f93f7991e7a2cbf9c649b3a
Daniel Koman <dankoman30@gmail.com>
2015-05-28 22:47:34 -0400

    SystemUI: vector support for navbar/pie icons
    ps: drawableToBitmap method
    
    ps: remove null return if vectordrawable (ImageHelper.java)
    
    Change-Id: I8279c19a6617edde3c5c1590683c6b593ec68e6c

--- core/java/com/android/internal/util/slim/ColorHelper.java
+++ core/java/com/android/internal/util/slim/ColorHelper.java
@@ -17,6 +17,7 @@
 package com.android.internal.util.slim;
 
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
@@ -24,11 +25,16 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.VectorDrawable;
 import android.graphics.drawable.Drawable;
 
 public class ColorHelper {
 
-    public static Bitmap getColoredBitmap(Drawable d, int color) {
+    public static Drawable getColoredDrawable(Drawable d, int color) {
+        if (d instanceof VectorDrawable) {
+            d.setTint(color);
+            return d;
+        }
         Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
         Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
         Paint pp = new Paint();
@@ -37,7 +43,19 @@ public class ColorHelper {
         pp.setColorFilter(frontFilter);
         Canvas cc = new Canvas(grayscaleBitmap);
         cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
-        return grayscaleBitmap;
+        return new BitmapDrawable(grayscaleBitmap);
+    }
+
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 
     private static Bitmap toGrayscale(Bitmap bmpOriginal) {
