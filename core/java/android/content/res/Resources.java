ec49a72540b9f9f3f7737af7e80ef7a1d57cd584
Roman Birg <roman@cyngn.com>
2015-06-03 11:52:16 -0700

    Density scaling: scale compatibility apps, wm command support
    Density scaling should work across all apps and different density
    configurations. It should also pass CTS.
    
    - force compatibility density if calculated compatibility density is
      different than what the user has requested
    
    - hook in "wm density" support, should behave the same as setting the
      density from Settings
    
    - clean up some logic
    
    Change-Id: I52c7c2ca200b78f9a5ee9170871ca62e1cab3bcb

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -250,7 +250,6 @@ public class Resources {
             CompatibilityInfo compatInfo, IBinder token) {
         mAssets = assets;
         mMetrics.setToDefaults();
-        mMetrics.updateDensity();
         if (compatInfo != null) {
             mCompatibilityInfo = compatInfo;
         }
@@ -1802,9 +1801,12 @@ public class Resources {
                 mConfiguration.setLayoutDirection(mConfiguration.locale);
             }
             if (mConfiguration.densityDpi != Configuration.DENSITY_DPI_UNDEFINED) {
-                mMetrics.densityDpi = mConfiguration.densityDpi;
-                mMetrics.density = mConfiguration.densityDpi * DisplayMetrics.DENSITY_DEFAULT_SCALE;
-                mMetrics.updateDensity();
+                if (DisplayMetrics.DENSITY_DEVICE_DEFAULT
+                        == mCompatibilityInfo.applicationDensity) {
+                    mMetrics.setDensity(DisplayMetrics.DENSITY_PREFERRED);
+                } else {
+                    mMetrics.setDensity(mCompatibilityInfo.applicationDensity);
+                }
             }
             mMetrics.scaledDensity = mMetrics.density * mConfiguration.fontScale;
 
@@ -2265,7 +2267,7 @@ public class Resources {
             }
             sPreloaded = true;
             mPreloading = true;
-            sPreloadedDensity = DisplayMetrics.getDeviceDensity();
+            sPreloadedDensity = DisplayMetrics.DENSITY_PREFERRED;
             mConfiguration.densityDpi = sPreloadedDensity;
             updateConfiguration(null, null);
         }
