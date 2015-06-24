bb75aaffdbcd9e2d39b778222008c2cfcae983cb
Kra1o5 <kra1o5x@gmail.com>
2015-06-24 14:37:42 +0200

    Set correct default action to longpress on the Recents (App Switch) button.
    Change-Id: I54aa49848baa56518758bbaf5f2feb0aba757cbb

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -7081,7 +7081,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         return action.equals(ActionConstants.ACTION_BACK)
                 || action.equals(ActionConstants.ACTION_MENU_BIG)
                 || action.equals(ActionConstants.ACTION_HOME)
-                || action.equals(ActionConstants.ACTION_SEARCH);
+                || action.equals(ActionConstants.ACTION_SEARCH)
+                || action.equals(ActionConstants.ACTION_RECENTS);
     }
 
     @Override
