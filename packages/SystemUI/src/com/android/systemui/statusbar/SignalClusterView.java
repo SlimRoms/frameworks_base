f0b2e47107474bb8a1de5516fa41796ca71cd50e
Danny Baumann <dannybaumann@web.de>
2015-06-29 09:10:42 +0200

    Don't inflate new phone states on callbacks.
    Due to the asynchronous nature of PhoneStateListeners, phone state
    callbacks for dropped and unregistered mobile signal controllers might
    come in even after unregistering. Don't make such callbacks create new
    signal bar instances, but instead drop them.
    
    Change-Id: I75afc0d283181208fea6a3699e2b95c0030ad6e2

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -177,7 +177,11 @@ public class SignalClusterView
             int activityIcon, int typeIcon, String contentDescription,
             String typeContentDescription, boolean isTypeIconWide,
             boolean showRoamingIndicator, int subId) {
-        PhoneState state = getOrInflateState(subId);
+        PhoneState state = getState(subId);
+        if (state == null) {
+            return;
+        }
+
         state.mMobileVisible = visible;
         state.mMobileStrengthId = strengthIcon;
         state.mMobileActivityId = activityIcon;
@@ -208,13 +212,13 @@ public class SignalClusterView
         }
     }
 
-    private PhoneState getOrInflateState(int subId) {
+    private PhoneState getState(int subId) {
         for (PhoneState state : mPhoneStates) {
             if (state.mSubId == subId) {
                 return state;
             }
         }
-        return inflatePhoneState(subId);
+        return null;
     }
 
     private PhoneState inflatePhoneState(int subId) {
