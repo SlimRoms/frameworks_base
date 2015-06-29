9ea7e346ab31f75be330349582f98eaa42529ab5
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-06-29 22:30:22 +0200

    [2/2] SlimHeadsUp: Add a global switch
    disabled : will disable all headsup
    enabled : notifications are shown as headsup per app (current behaviour)
    
    Change-Id: I1dc1b4c84d601149f21eca0722fc1f586d33ba3e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -207,6 +207,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected boolean mDisableNotificationAlerts = false;
 
     private int mHeadsUpSnoozeTime;
+    private boolean mHeadsUpGlobalSwitch;
     private long mHeadsUpSnoozeStartTime;
     protected String mHeadsUpPackageName;
 
@@ -994,10 +995,10 @@ public abstract class BaseStatusBar extends SystemUI implements
             if (isThisASystemPackage(pkg, pmUser)) {
                 headsUpButton.setVisibility(View.GONE);
             } else {
-                boolean isHeadsUpEnabled = mNoMan.getHeadsUpNotificationsEnabledForPackage(
+                boolean isHeadsUpEnabledForPackage = mNoMan.getHeadsUpNotificationsEnabledForPackage(
                         pkg, appUidF) != Notification.HEADS_UP_NEVER;
-                headsUpButton.setAlpha(isHeadsUpEnabled ? 1f : 0.5f);
-                setHeadsUpButtonContentDescription((View) headsUpButton, isHeadsUpEnabled);
+                headsUpButton.setAlpha(isHeadsUpEnabledForPackage ? 1f : 0.5f);
+                setHeadsUpButtonContentDescription((View) headsUpButton, isHeadsUpEnabledForPackage);
                 headsUpButton.setVisibility(View.VISIBLE);
                 headsUpButton.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
@@ -2315,6 +2316,14 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mHeadsUpSnoozeTime / 60 / 1000), Toast.LENGTH_LONG).show();
     }
 
+    protected boolean isHeadsUpEnabled() {
+        return mHeadsUpGlobalSwitch;
+    }
+
+    protected void setHeadsUpEnabled(boolean headsUpGlobalSwitch) {
+        mHeadsUpGlobalSwitch = headsUpGlobalSwitch;
+    }
+
     protected boolean isHeadsUpInSnooze() {
         return (mHeadsUpSnoozeStartTime + mHeadsUpSnoozeTime - System.currentTimeMillis()) > 0;
     }
@@ -2342,6 +2351,11 @@ public abstract class BaseStatusBar extends SystemUI implements
             if (DEBUG) {
                 Log.d(TAG, "Skipping HUN check for " + sbn.getKey() + " since it's filtered out.");
             }
+            return false;
+        }
+
+        // Stop here if headsup is globally disabled
+        if (!isHeadsUpEnabled()) {
             return false;
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -484,6 +484,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.HEADS_UP_SNOOZE_TIME),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_GLOBAL_SWITCH),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -534,6 +537,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         R.integer.heads_up_notification_decay),
                         UserHandle.USER_CURRENT);
                 resetHeadsUpDecayTimer();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_GLOBAL_SWITCH))) {
+                final boolean headsUpGlobalSwitch = Settings.System.getIntForUser(
+                        mContext.getContentResolver(),
+                        Settings.System.HEADS_UP_GLOBAL_SWITCH,
+                        1,
+                        UserHandle.USER_CURRENT) == 1;
+                setHeadsUpEnabled(headsUpGlobalSwitch);
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS))) {
                 attachPieContainer(isPieEnabled());
