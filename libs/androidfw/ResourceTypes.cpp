b95de1223fce8f705c71a13c29841802037708d4
Adnan Begovic <adnan@cyngn.com>
2015-07-06 20:06:36 -0700

    androidfw: Declare and load slim framework resource package w/ id.
    Change-Id: I63b8f3e4a938896dd21999c5b4470573a1da2e52

--- libs/androidfw/ResourceTypes.cpp
+++ libs/androidfw/ResourceTypes.cpp
@@ -62,6 +62,7 @@ namespace android {
 #define IDMAP_CURRENT_VERSION   0x00000001
 
 #define APP_PACKAGE_ID      0x7f
+#define SLIM_PACKAGE_ID    0x37
 #define SYS_PACKAGE_ID      0x01
 
 static const bool kDebugStringPoolNoisy = false;
@@ -6590,6 +6591,7 @@ DynamicRefTable::DynamicRefTable(uint8_t packageId, bool appAsLib)
     // Reserved package ids
     mLookupTable[APP_PACKAGE_ID] = APP_PACKAGE_ID;
     mLookupTable[SYS_PACKAGE_ID] = SYS_PACKAGE_ID;
+    mLookupTable[SLIM_PACKAGE_ID] = SLIM_PACKAGE_ID;
 }
 
 status_t DynamicRefTable::load(const ResTable_lib_header* const header)
@@ -6920,7 +6922,6 @@ bool ResTable::getIdmapInfo(const void* idmap, size_t sizeBytes,
     }
     return true;
 }
-
 
 #define CHAR16_TO_CSTR(c16, len) (String8(String16(c16,len)).string())
 
