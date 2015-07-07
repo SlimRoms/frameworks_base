a5c632356c7898a434eb0575dce7b58dc0857afc
Adnan Begovic <adnan@cyngn.com>
2015-07-06 20:06:36 -0700

    androidfw: Declare and load slim framework resource package w/ id.
    Change-Id: I63b8f3e4a938896dd21999c5b4470573a1da2e52

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -65,6 +65,7 @@ static const char* kDefaultVendor = "default";
 static const char* kAssetsRoot = "assets";
 static const char* kAppZipName = NULL; //"classes.jar";
 static const char* kSystemAssets = "framework/framework-res.apk";
+static const char* kSlimFrameworkAssets = "framework/org.slim.framework-res.apk";
 static const char* kResourceCache = "resource-cache";
 
 static const char* kExcludeExtension = ".EXCLUDE";
@@ -322,7 +323,16 @@ bool AssetManager::addDefaultAssets()
     String8 path(root);
     path.appendPath(kSystemAssets);
 
-    return addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
+    bool ret = addAssetPath(path, NULL, false /* appAsLib */, true /* isSystemAsset */);
+    if (ret) {
+        String8 pathSlim(root);
+        pathSlim.appendPath(kSlimFrameworkAssets);
+
+        if (!addAssetPath(pathSlim, NULL, false /* appAsLib */, false /*isSystemAsset */)) {
+            ALOGE("Failed to load Slim framework resources");
+        }
+    }
+    return ret;
 }
 
 int32_t AssetManager::nextAssetPath(const int32_t cookie) const
