d958d59fe294114905c88ce9efa84593f634cbc2
Roman Birg <roman@cyngn.com>
2015-08-12 12:56:31 -0700

    SystemUI: fix empty notification space in shade_locked
    In I806d1fd8eac73c4af0820319d127423ae6467f60, we started
    funneling update notification calls through the handler
    to avoid queueing up hundreds of these calls if a
    notification became spammy and to only execute one update if many were
    queued.
    
    However, some animations and state transitions are expecting the action
    to be done immediately after the call. Facilitate these requests by
    allowing them to bypass the handler if they want.
    
    Change-Id: Ice14cdaf93a4d370779b4a3e8996d17cd42fdff8
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2017,9 +2017,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     @Override
-    protected void updateNotifications() {
-        if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
-            mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
+    protected void updateNotifications(boolean immediate) {
+        if (immediate) {
+            mHandler.removeMessages(MSG_UPDATE_NOTIFICATIONS);
+            handleUpdateNotifications();
+        } else {
+            if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
+                mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
+            }
         }
     }
 
@@ -3862,7 +3867,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         super.userSwitched(newUserId);
         if (MULTIUSER_DEBUG) mNotificationPanelDebugText.setText("USER " + newUserId);
         animateCollapsePanels();
-        updateNotifications();
+        updatePublicMode();
+        updateNotifications(true);
         resetUserSetupObserver();
         setControllerUsers();
     }
@@ -4449,7 +4455,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDozingState();
         updatePublicMode();
         updateStackScrollerState(goingToFullShade);
-        updateNotifications();
+        updateNotifications(true);
         checkBarModes();
         updateCarrierLabelVisibility(false);
         updateMediaMetaData(false);
