9f052fa992eb16f1e1501ba91f4c9b4afb0df25d
Josue Rivera <prbassplayer@gmail.com>
2015-08-24 13:04:45 -0600

    Revert "Framework/Base: PerfLock for browser drag/fling"
    This reverts commit 393bf2a64f0940a1428a153fb329b7975f06ca85.
    
    Change-Id: I77b57bda2f2c383457c8f05f0a8f107de8888d71
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -101,7 +101,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.HashMap;
-import org.codeaurora.Performance;
 
 /**
  * An activity is a single, focused thing that the user can do.  Almost all
@@ -663,16 +662,7 @@ public class Activity extends ContextThemeWrapper
         Window.Callback, KeyEvent.Callback,
         OnCreateContextMenuListener, ComponentCallbacks2,
         Window.OnWindowDismissedCallback {
-
-    private static Performance mPerf = null;
-    private String[] mPerfActivityList = null;
-    private static int mDragBoostPossible = -1;
-    private static int mPerfLockDuration = -1;
-    private static int mAsCpuBoost = -1;
-    private static int mAsSchedBoost = -1;
-    private static int mAsPcDisblBoost = -1;
     private static final String TAG = "Activity";
-
     private static final boolean DEBUG_LIFECYCLE = false;
 
     /** Standard activity result: operation canceled. */
@@ -948,18 +938,6 @@ public class Activity extends ContextThemeWrapper
         if (mVoiceInteractor != null) {
             mVoiceInteractor.attachActivity(this);
         }
-
-        mPerfActivityList = getResources().getStringArray(
-                    com.android.internal.R.array.boost_activityList);
-        mPerfLockDuration = getResources().getInteger(
-                    com.android.internal.R.integer.ascrollboost_timeout);
-        mAsCpuBoost = getResources().getInteger(
-                    com.android.internal.R.integer.ascrollboost_cpuboost);
-        mAsSchedBoost = getResources().getInteger(
-                    com.android.internal.R.integer.ascrollboost_schedboost);
-        mAsPcDisblBoost = getResources().getInteger(
-                    com.android.internal.R.integer.ascrollboost_pcdisbl);
-
         mCalled = true;
     }
 
@@ -2758,28 +2736,6 @@ public class Activity extends ContextThemeWrapper
      * @return boolean Return true if this event was consumed.
      */
     public boolean dispatchTouchEvent(MotionEvent ev) {
-
-        if(mDragBoostPossible == -1) {
-            mDragBoostPossible = 0;
-            String currentActivity = getPackageName();
-            if(mPerfActivityList != null){
-                for (String match : mPerfActivityList) {
-                    if (currentActivity.indexOf(match) != -1){
-                        mDragBoostPossible = 1;
-                        break;
-                    }
-                }
-            }
-        }
-        if (mDragBoostPossible == 1) {
-            if (mPerf == null){
-                mPerf = new Performance();
-            }
-            mPerf.perfLockAcquireTouch(ev,
-                getResources().getDisplayMetrics(),
-                mPerfLockDuration,
-                mAsSchedBoost, mAsCpuBoost, mAsPcDisblBoost);
-        }
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
             onUserInteraction();
         }
