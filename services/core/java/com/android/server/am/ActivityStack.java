d469ee8f9c1096f310f3ba7684aff263a4adab28
Steve Kondik <steve@cyngn.com>
2015-10-31 03:22:07 -0700

    Remove Qualcomm BoostFramework
     * This is gonna get really sketchy with the proprietary bits, and
       we have our own solution. Revert all associated patches.
    
    Revert "BoostFramework: Adding config for disable packing params & scroll fix"
    
    This reverts commit a313bfae1213dcf8e0845e90f260aada5026685e.
    
    Revert "Perf: Adding hooks for IO prefetcher into framework"
    
    This reverts commit 96e75fcafbeb77427ff4407e80a5d05bf92ec732.
    
    Revert "frameworks/base: Add support for low resolution rendering"
    
    This reverts commit d5282e055963a44df64265e4a592750e31bd9901.
    
    Revert "frameworks/base: support for loading wfd whitelist dynamic libraries"
    
    This reverts commit 79a889bcf6e4c96db48bfb755b7c7e100f4800b1.
    
    Revert "ActivityTrigger: New class to invoke when activity starts/resumes"
    
    This reverts commit 20d6e3c9b06d790e5ede4b1aa585d43a7d72b560.
    
    Revert "BoostFramework to enchance performance during critical scenarios"
    
    This reverts commit 0c3a49a56f827e16d038542b0a81c0e257787995.
    
    Change-Id: Ibc81f3150f4bea572f869be688ebfade11b27307

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -66,8 +66,6 @@ import android.service.voice.IVoiceInteractionSession;
 import android.util.EventLog;
 import android.util.Slog;
 import android.view.Display;
-import android.util.BoostFramework;
-import com.android.internal.app.ActivityTrigger;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -153,10 +151,6 @@ final class ActivityStack {
     final WindowManagerService mWindowManager;
     private final RecentTasks mRecentTasks;
 
-    public BoostFramework mPerf = null;
-    public boolean mIsAnimationBoostEnabled = false;
-    public int aBoostTimeOut = 0;
-    public int aBoostParamVal[];
     /**
      * The back history of all previous (and possibly still
      * running) activities.  It contains #TaskRecord objects.
@@ -272,8 +266,6 @@ final class ActivityStack {
 
     final Handler mHandler;
 
-    static final ActivityTrigger mActivityTrigger = new ActivityTrigger();
-
     final class ActivityStackHandler extends Handler {
 
         ActivityStackHandler(Looper looper) {
@@ -370,14 +362,6 @@ final class ActivityStack {
         mCurrentUser = mService.mCurrentUserId;
         mRecentTasks = recentTasks;
         mOverrideConfig = Configuration.EMPTY;
-        mIsAnimationBoostEnabled = mService.mContext.getResources().getBoolean(
-                   com.android.internal.R.bool.config_enablePerfBoostForAnimation);
-        if (mIsAnimationBoostEnabled) {
-           aBoostTimeOut = mService.mContext.getResources().getInteger(
-                   com.android.internal.R.integer.animationboost_timeout_param);
-           aBoostParamVal = mService.mContext.getResources().getIntArray(
-                   com.android.internal.R.array.animationboost_param_value);
-        }
     }
 
     boolean okToShowLocked(ActivityRecord r) {
@@ -1698,8 +1682,6 @@ final class ActivityStack {
 
         if (DEBUG_SWITCH) Slog.v(TAG_SWITCH, "Resuming " + next);
 
-        mActivityTrigger.activityResumeTrigger(next.intent, next.info, next.appInfo);
-
         // If we are currently pausing an activity, then don't do anything
         // until that is done.
         if (!mStackSupervisor.allPausedActivitiesComplete()) {
@@ -1821,9 +1803,6 @@ final class ActivityStack {
         // that the previous one will be hidden soon.  This way it can know
         // to ignore it when computing the desired screen orientation.
         boolean anim = true;
-        if (mIsAnimationBoostEnabled == true && mPerf == null) {
-            mPerf = new BoostFramework();
-        }
         if (prev != null) {
             if (prev.finishing) {
                 if (DEBUG_TRANSITION) Slog.v(TAG_TRANSITION,
@@ -1835,9 +1814,6 @@ final class ActivityStack {
                     mWindowManager.prepareAppTransition(prev.task == next.task
                             ? AppTransition.TRANSIT_ACTIVITY_CLOSE
                             : AppTransition.TRANSIT_TASK_CLOSE, false);
-                    if(prev.task != next.task && mPerf != null) {
-                       mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
-                    }
                 }
                 mWindowManager.setAppWillBeHidden(prev.appToken);
                 mWindowManager.setAppVisibility(prev.appToken, false);
@@ -1853,9 +1829,6 @@ final class ActivityStack {
                             : next.mLaunchTaskBehind
                                     ? AppTransition.TRANSIT_TASK_OPEN_BEHIND
                                     : AppTransition.TRANSIT_TASK_OPEN, false);
-                    if(prev.task != next.task && mPerf != null) {
-                        mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
-                    }
                 }
             }
             if (false) {
@@ -2168,7 +2141,6 @@ final class ActivityStack {
         task.setFrontOfTask();
 
         r.putInHistory();
-        mActivityTrigger.activityStartTrigger(r.intent, r.info, r.appInfo);
         if (!isHomeStack() || numActivities() > 0) {
             // We want to show the starting preview window if we are
             // switching to a new task, or the next activity's process is
