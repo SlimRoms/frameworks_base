9f2bf7870c2a8c0c84e1ae3f0bf23a6f10ad28a6
Thecrazyskull <anaskarbila@gmail.com>
2015-11-15 09:47:55 +0100

    base: remove duplicate import
    Change-Id: Ib217fc11eb9d576fff161c2f481daf7ef9d2847a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -72,7 +72,6 @@ import android.media.session.MediaSessionManager;
 import android.media.session.PlaybackState;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
