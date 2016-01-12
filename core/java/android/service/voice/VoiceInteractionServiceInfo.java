533f9d472ca44106a2d582a9c5ca0b65921a8d3a
Griffin Millender <griffinn.millender@gmail.com>
2016-01-12 10:14:57 -0600

    Fix fatal boot error
    Change-Id: Ic56ac00773cf4cb3fe35a00ea03caf98520e2a47

--- core/java/android/service/voice/VoiceInteractionServiceInfo.java
+++ core/java/android/service/voice/VoiceInteractionServiceInfo.java
@@ -58,6 +58,7 @@ public class VoiceInteractionServiceInfo {
     }
 
     public VoiceInteractionServiceInfo(PackageManager pm, ServiceInfo si) {
+        if (si == null) return;
         if (!Manifest.permission.BIND_VOICE_INTERACTION.equals(si.permission)) {
             mParseError = "Service does not require permission "
                     + Manifest.permission.BIND_VOICE_INTERACTION;
