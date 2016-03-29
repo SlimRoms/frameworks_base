a42cb503e686d57475fa3ca78deca26ab4c906de
Griffin Millender <griffinn.millender@gmail.com>
2016-03-29 11:16:39 -0500

    Add SlimMetricsLogger
    Change-Id: Ie8024c0857897b18990d2f40c19843bb4d26923c

---
+++ core/java/com/android/internal/logging/SlimMetricsLogger.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2016, The SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.logging;
+
+/**
+ * Serves as a central location for logging constants that is android release agnostic.
+ */
+public class SlimMetricsLogger extends MetricsLogger {
+
+    private static final int BASE = -Integer.MAX_VALUE;
+
+    public static final int INTERFACE_SETTINGS = BASE + 2;
+    public static final int STATUS_BAR_SETTINGS = BASE + 3;
+}
