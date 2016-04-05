12b5c12a5521cda27cf889dc73a8c01d90fc8f0e
Griffin Millender <griffinn.millender@gmail.com>
2016-04-05 11:29:22 -0500

    Add helper functions for slim actions
    Change-Id: Id33bf9d68b43254d20eeab9aee6afe5f05237925

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5601,6 +5601,12 @@ public class WindowManagerService extends IWindowManager.Stub
         }
     }
 
+    /* @hide */
+     @Override
+    public void toggleGlobalMenu() {
+        mPolicy.toggleGlobalMenu();
+    }
+
     void dispatchNewAnimatorScaleLocked(Session session) {
         mH.obtainMessage(H.NEW_ANIMATOR_SCALE, session).sendToTarget();
     }
