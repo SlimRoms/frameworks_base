facc97998688d52a5066ffb87ca7a8fe6d023586
Martinusbe <martinusbe@gmail.com>
2016-07-11 18:18:49 +0200

    DayNight: include SystemUI & AlertDialog
    Change-Id: I606a4bdec554b629937d5b26153b01b8372c3027

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6904,7 +6904,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             PackageManager.FEATURE_TELEVISION)) {
                         theme = com.android.internal.R.style.Theme_Leanback_Dialog_Alert;
                     } else {
-                        theme = 0;
+                        theme = 6;
                     }
 
                     mBootMsgDialog = new ProgressDialog(mContext, theme) {
