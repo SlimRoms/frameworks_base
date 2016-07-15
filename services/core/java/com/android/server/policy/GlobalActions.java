1f724845fdb8917d23466547952aba90bbc8756e
Martinusbe <martinusbe@gmail.com>
2016-07-15 17:51:17 +0200

    DayNightTheme fixes
    -doze
    -notification
    -globalactions
    
    Uses code and resources from 
      Frameworks/Systemui DayNight update (http://tinyurl.com/zfzrqhq)
      Frameworks - DayNight ambient display support http://tinyurl.com/h8cb88j
      SystemUI - DayNight notification actions/global actions update http://tinyurl.com/h256u5z
      Dark Mode Theming Changes http://tinyurl.com/hzwfqkj
    
    Change-Id: I8b5ea27d7bbc0e78073e8a9e9655128398743862
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -1162,7 +1162,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         private boolean mCancelOnUp;
 
         public GlobalActionsDialog(Context context, AlertParams params) {
-            super(context, getDialogTheme(context));
+            super(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert);
             mContext = getContext();
             mAlert = new AlertController(mContext, this, getWindow());
             mAdapter = (MyAdapter) params.mAdapter;
