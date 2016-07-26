99777641480bd0ad9149764248ccedd1856a9d65
BestPig <bestpig82@gmail.com>
2016-07-26 21:35:55 +0200

    Add USB Tether QS Tile
    Change-Id: If3ba223823e064faf253b6b8a5c5dc56e7a7c772

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -202,6 +202,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("location")) return R.string.quick_settings_location_label;
         else if (spec.equals("cast")) return R.string.quick_settings_cast_title;
         else if (spec.equals("hotspot")) return R.string.quick_settings_hotspot_label;
+        else if (spec.equals("usb_tether")) return R.string.quick_settings_usb_tether_label;
         return 0;
     }
 
@@ -441,6 +442,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("location")) return R.drawable.ic_signal_location_enable;
             else if (mSpec.equals("cast")) return R.drawable.ic_qs_cast_on;
             else if (mSpec.equals("hotspot")) return R.drawable.ic_hotspot_enable;
+            else if (mSpec.equals("usb_tether")) return R.drawable.ic_qs_usb_tether_off;
             return R.drawable.android;
         }
 
