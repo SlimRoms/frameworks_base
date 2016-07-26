c21127b325a8c7b753dee5aa37c14378319d0050
Danesh Mondegarian <daneshm90@gmail.com>
2016-07-26 23:39:45 +0200

    Add Screen timeout tile
    SystemUI : Add screen timeout tile
    Squash of the Following:
    *SystemUI: Fix screen timeout tile title
    *ScreenOrientationTile : Add instanceof checks for AnimatedVectorDrawables
    *qs: support for all timeout value in case set by user apps
    
    PureNexus edits:
    *adapt for marshmallow
    *fix strings
    *fix drawables
    
    Change-Id: Ia674f62aec21349d924c9850099aa13142e82add

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.statusbar.policy.BluetoothController;
 import com.android.systemui.statusbar.policy.CastController;
@@ -272,6 +273,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("screenoff")) return new ScreenOffTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
+        else if (tileSpec.equals("timeout")) return new ScreenTimeoutTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
