98f4f05bb7b71234504b537c7c77646720cbf63a
Keith Mok <kmok@cyngn.com>
2016-07-27 20:48:38 +0200

    SystemUI: add caffeine qs tile
    Change-Id: I4d3b7ce9782f56ec862eaead3621f8a53f706a82

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -258,6 +258,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("ime")) return R.string.quick_settings_ime_label;
         else if (spec.equals("volume")) return R.string.quick_settings_volume_panel_label;
         else if (spec.equals("sound")) return R.string.quick_settings_sound_label;
+        else if (spec.equals("caffeine")) return R.string.quick_settings_caffeine_label;
         return 0;
     }
 
@@ -535,6 +536,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("ime")) return R.drawable.ic_qs_ime;
             else if (mSpec.equals("volume")) return R.drawable.ic_qs_volume_panel;
             else if (mSpec.equals("sound")) return R.drawable.ic_qs_ringer_audible;
+            else if (mSpec.equals("caffeine")) return R.drawable.ic_qs_caffeine_on;
             return R.drawable.android;
         }
 
