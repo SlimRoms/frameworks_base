3794c2d7810cfc2d151a4bd6b6b9c7770b366589
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-07-27 20:48:38 +0200

    Add Sync QS Tile
    Change-Id: I9cae702d1a36742f0cd2bd71694fe1dbf5fbad8d

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -247,6 +247,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("usb_tether")) return R.string.quick_settings_usb_tether_label;
         else if (spec.equals("ambient_display")) return R.string.quick_settings_ambient_display_label;
         else if (spec.equals("screenshot")) return R.string.quick_settings_screenshot_label;
+        else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
         return 0;
     }
 
@@ -513,6 +514,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("usb_tether")) return R.drawable.ic_qs_usb_tether_off;
             else if (mSpec.equals("ambient_display")) return R.drawable.ic_qs_ambientdisplay_on;
             else if (mSpec.equals("screenshot")) return R.drawable.ic_qs_screenshot;
+            else if (mSpec.equals("sync")) return R.drawable.ic_qs_sync_on;
             return R.drawable.android;
         }
 
