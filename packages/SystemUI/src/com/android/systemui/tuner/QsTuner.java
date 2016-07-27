3a240895d69e344786964c6b466f9c3f9cfdc1be
Danesh Mondegarian <daneshm90@gmail.com>
2016-07-27 20:48:38 +0200

    Add Screen timeout tile
    SystemUI : Add screen timeout tile
    Squash of the Following:
    *SystemUI: Fix screen timeout tile title
    *ScreenOrientationTile : Add instanceof checks for AnimatedVectorDrawables
    *qs: support for all timeout value in case set by user apps
    
    PureNexus edits:
    *adapt for marshmallow
    *fix strings
    *fix drawables
    
    Change-Id: Ia674f62aec21349d924c9850099aa13142e82add

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -249,6 +249,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("screenshot")) return R.string.quick_settings_screenshot_label;
         else if (spec.equals("screenoff")) return R.string.quick_settings_screen_off;
         else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
+        else if (spec.equals("timeout")) return R.string.quick_settings_timeout_label;
         return 0;
     }
 
@@ -517,6 +518,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("screenshot")) return R.drawable.ic_qs_screenshot;
             else if (mSpec.equals("screenoff")) return R.drawable.ic_qs_power;
             else if (mSpec.equals("sync")) return R.drawable.ic_qs_sync_on;
+            else if (mSpec.equals("timeout")) return R.drawable.ic_qs_screen_timeout_vector;
             return R.drawable.android;
         }
 
