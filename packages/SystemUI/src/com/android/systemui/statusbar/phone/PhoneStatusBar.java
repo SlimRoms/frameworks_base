2b7adf0096852e4c1d1dcf58a3d6a1d97738f5f3
Daniel Koman <dankoman30@gmail.com>
2016-08-05 23:22:34 +0200

    base: fix keyguard in recreatestatusbar
    Change-Id: I124175849c03d0b3a2e988a4c5df5e451283e64b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3469,16 +3469,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         });
 
-        // if the keyguard was showing while this change occurred we'll need to do some extra work
-        if (mState == StatusBarState.KEYGUARD) {
-            // this will make sure the keyguard is showing
-            showKeyguard();
-            // make sure to hide the notification icon area and system iconography
-            // to avoid overlap (CYNGNOS-2253)
-            mIconController.hideNotificationIconArea(false);
-            mIconController.hideSystemIconArea(false);
-        }
-
         // restart the keyguard so it picks up the newly created ScrimController
         startKeyguard();
 
@@ -3486,6 +3476,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mState == StatusBarState.KEYGUARD) {
             // this will make sure the keyguard is showing
             showKeyguard();
+            // make sure to hide the notification icon area and system iconography
+            mIconController.hideNotificationIconArea(false);
+            mIconController.hideSystemIconArea(false);
         }
     }
 
