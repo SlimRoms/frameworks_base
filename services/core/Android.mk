a76c036afd068d7f00aaf2d708b6b2245b63c857
Griffin Millender <griffinn.millender@gmail.com>
2016-08-12 17:57:30 -0500

    Add back navbar stuff to PhoneWindowManager   * fixes transparency issues   * fixes dimesnion options not working   * fixes show in landscape option
    Change-Id: I3a64c6a36cc419e647ae70bef452c8e3466061fc

--- services/core/Android.mk
+++ services/core/Android.mk
@@ -16,7 +16,7 @@ LOCAL_SRC_FILES += \
 LOCAL_AIDL_INCLUDES += \
     system/netd/server/binder
 
-LOCAL_JAVA_LIBRARIES := services.net telephony-common
+LOCAL_JAVA_LIBRARIES := services.net telephony-common org.slim.framework
 LOCAL_STATIC_JAVA_LIBRARIES := tzdata_update
 
 ifneq ($(INCREMENTAL_BUILDS),)
