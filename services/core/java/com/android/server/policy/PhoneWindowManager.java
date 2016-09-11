3368bd20fe76ee304e53d99a03448163e8498f95
Steve Kondik <steve@cyngn.com>
2016-09-11 00:49:41 -0700

    policy: Use PathClassLoader for loading the keyhandler
     * Fix crash on start due to getCacheDir throwing an exception.
     * We can't do this anymore due to the new storage/crypto in N.
    
    Change-Id: I28426a5df824460ebc74aa19068192adb00d4f7c

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -155,8 +155,6 @@ import com.android.server.policy.keyguard.KeyguardServiceDelegate;
 import com.android.server.policy.keyguard.KeyguardServiceDelegate.DrawnListener;
 import com.android.server.statusbar.StatusBarManagerInternal;
 
-import dalvik.system.DexClassLoader;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -167,6 +165,8 @@ import java.lang.reflect.Constructor;
 
 import org.slim.framework.internal.policy.HardwareKeyHandler;
 import org.slim.provider.SlimSettings;
+
+import dalvik.system.PathClassLoader;
 
 /**
  * WindowManagerPolicy implementation for the Android phone UI.  This
@@ -1832,10 +1832,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.string.config_deviceKeyHandlerClass);
 
         if (!deviceKeyHandlerLib.isEmpty() && !deviceKeyHandlerClass.isEmpty()) {
-            DexClassLoader loader =  new DexClassLoader(deviceKeyHandlerLib,
-                    new ContextWrapper(mContext).getCacheDir().getAbsolutePath(),
-                    null,
-                    ClassLoader.getSystemClassLoader());
+            PathClassLoader loader =  new PathClassLoader(deviceKeyHandlerLib,
+                    getClass().getClassLoader());
             try {
                 Class<?> klass = loader.loadClass(deviceKeyHandlerClass);
                 Constructor<?> constructor = klass.getConstructor(Context.class);
