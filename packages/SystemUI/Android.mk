b3637187095b5802b5e396f1aa31be00dd2f8930
Griffin Millender <griffinn.millender@gmail.com>
2016-09-26 00:37:40 +0200

    Add slimsystemui
    Change-Id: Ib03f3715e2e71523074d759d72cf1b0fe5b35a7b

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -56,6 +56,12 @@ ifneq ($(SYSTEM_UI_INCREMENTAL_BUILDS),)
     LOCAL_JACK_ENABLED := incremental
 endif
 
+# Slim Framework
+LOCAL_JAVA_LIBRARIES += org.slim.framework
+LOCAL_FULL_LIBS_MANIFEST_FILES := frameworks/opt/slim/packages/SlimSystemUI/AndroidManifest.xml
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../opt/slim/packages/SlimSystemUI/src)
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/../../../../frameworks/opt/cards/res
+
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
