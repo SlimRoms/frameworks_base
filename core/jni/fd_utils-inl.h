3dbfc797a53746d1d0a0cb3db0fae08f7b597cd1
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-10 22:07:59 +0100

    zygote: Allow device to append extra whitelisted paths
    Change-Id: Ic5b056d7ead520da8648db9be28e16a81ec27c73

--- core/jni/fd_utils-inl.h
+++ core/jni/fd_utils-inl.h
@@ -35,6 +35,8 @@
 #include "JNIHelp.h"
 #include "ScopedPrimitiveArray.h"
 
+#include <fd_utils-inl-extra.h>
+
 // Whitelist of open paths that the zygote is allowed to keep open.
 //
 // In addition to the paths listed here, all files ending with
@@ -60,7 +62,10 @@ static const char* kPathWhitelist[] = {
   "/dev/rpmsg-omx0",
   "/dev/rpmsg-omx1",
   "@netlink@",
-  "/proc/ged"
+  "/proc/ged",
+#ifdef PATH_WHITELIST_EXTRA_H
+PATH_WHITELIST_EXTRA_H
+#endif
 };
 
 static const char* kFdPath = "/proc/self/fd";
