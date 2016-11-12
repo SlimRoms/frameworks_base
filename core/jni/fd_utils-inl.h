81760e4b9026c3b3153a8e6691494484c7b92897
Ricardo Cerqueira <android@cerqueira.org>
2016-11-12 15:27:32 +0000

    zygote: Add /proc/ged to the FD whitelist
    This is used by some platforms' gfx stack
    
    Change-Id: Ife6e1b0df30fc23eff7fb6a16ff95c3e7b16a9c7

--- core/jni/fd_utils-inl.h
+++ core/jni/fd_utils-inl.h
@@ -59,7 +59,8 @@ static const char* kPathWhitelist[] = {
   "/dev/ion",
   "/dev/rpmsg-omx0",
   "/dev/rpmsg-omx1",
-  "@netlink@"
+  "@netlink@",
+  "/proc/ged"
 };
 
 static const char* kFdPath = "/proc/self/fd";
