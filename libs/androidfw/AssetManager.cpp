39ddc58d5a77952beb81796560d4aa81ce102302
Martinusbe <martinusbe@gmail.com>
2017-01-10 18:00:45 +0100

    Revert "RRO: Synchronize access to overlays.list"
    This reverts commit 0fbb60811076e6fcfd576287b6e6a16ac1a69c44.
    
    Change-Id: I347bc48548b8d5ea671576dee5d48e62b84bf7bc

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -35,9 +35,6 @@
 #include <utils/threads.h>
 #include <utils/Timers.h>
 #include <utils/Trace.h>
-#ifndef _WIN32
-#include <sys/file.h>
-#endif
 
 #include <assert.h>
 #include <dirent.h>
@@ -780,12 +777,6 @@ void AssetManager::addSystemOverlays(const char* pathOverlaysList,
         return;
     }
 
-#ifndef _WIN32
-    if (TEMP_FAILURE_RETRY(flock(fileno(fin), LOCK_SH)) != 0) {
-        fclose(fin);
-        return;
-    }
-#endif
     char buf[1024];
     while (fgets(buf, sizeof(buf), fin)) {
         // format of each line:
@@ -817,10 +808,6 @@ void AssetManager::addSystemOverlays(const char* pathOverlaysList,
             delete oidmap;
         }
     }
-
-#ifndef _WIN32
-    TEMP_FAILURE_RETRY(flock(fileno(fin), LOCK_UN));
-#endif
     fclose(fin);
 }
 
