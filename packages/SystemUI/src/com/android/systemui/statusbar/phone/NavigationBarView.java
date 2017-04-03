280e904a559a27cb005f31d07a1fd7a5f15d10c8
Griffin Millender <griffinn.millender@gmail.com>
2017-04-03 18:30:56 -0500

    Merge android-7.1.2_r2
    Conflicts:
    	cmds/bootanimation/BootAnimation.cpp
    	cmds/bootanimation/BootAnimation.h
    	core/java/android/app/ActivityManagerInternal.java
    	core/java/android/bluetooth/BluetoothAdapter.java
    	core/java/android/bluetooth/BluetoothProfile.java
    	core/java/android/content/pm/PackageManagerInternal.java
    	core/jni/android_util_AssetManager.cpp
    	core/res/res/values-zh-rCN/strings.xml
    	core/res/res/values/config.xml
    	core/tests/utiltests/Android.mk
    	libs/androidfw/AssetManager.cpp
    	packages/SystemUI/res/values/config.xml
    	services/core/java/com/android/server/BluetoothManagerService.java
    	services/core/java/com/android/server/am/ActivityManagerService.java
    	services/core/java/com/android/server/pm/PackageManagerService.java
    	services/core/java/com/android/server/power/PowerManagerService.java
    	telecomm/java/android/telecom/ParcelableCall.java
    
    Change-Id: I5f9767fb52d8d4458bd518f4381103a6ce990ccd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -565,6 +565,7 @@ public class NavigationBarView extends LinearLayout {
         getImeSwitchButton().setOnClickListener(mImeSwitcherClickListener);
 
         mDeadZone = (DeadZone) mCurrentView.findViewById(R.id.deadzone);
+        mDeadZone.setDisplayRotation(mCurrentRotation);
 
         // force the low profile & disabled states into compliance
         mBarTransitions.init();
@@ -577,6 +578,8 @@ public class NavigationBarView extends LinearLayout {
 
         updateTaskSwitchHelper();
         setNavigationIconHints(mNavigationIconHints, true);
+
+        getHomeButton().setVertical(mVertical);
     }
 
     private void updateTaskSwitchHelper() {
