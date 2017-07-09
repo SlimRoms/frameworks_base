95b5061e5ce187b5917f34fce1950e48769138e1
Josue Rivera <prbassplayer@gmail.com>
2017-07-09 11:50:02 -0400

    Revert "Allow adjusting progress on touch events."
    This reverts commit 41a1bcdfb264eb54dd49907e6a994d7e8ee34082.
    
    Change-Id: I8b489d09e851bf461f184a5c579f18bf2e4dc7b2
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1822,7 +1822,7 @@ public final class PowerManagerService extends SystemService
                             keyboardBrightness = mKeyboardBrightness;
                         }
 
-                        //mKeyboardLight.setBrightness(mKeyboardVisible ? keyboardBrightness : 0);
+                        mKeyboardLight.setBrightness(mKeyboardVisible ? keyboardBrightness : 0);
                         if (mButtonTimeout != 0 && now > mLastUserActivityTime + mButtonTimeout) {
                              mButtonsLight.setBrightness(0);
                         } else {
@@ -1836,7 +1836,7 @@ public final class PowerManagerService extends SystemService
                         nextTimeout = mLastUserActivityTime + screenOffTimeout;
                         if (now < nextTimeout) {
                             mButtonsLight.setBrightness(0);
-                            //mKeyboardLight.setBrightness(0);
+                            mKeyboardLight.setBrightness(0);
                             mUserActivitySummary = USER_ACTIVITY_SCREEN_DIM;
                         }
                     }
