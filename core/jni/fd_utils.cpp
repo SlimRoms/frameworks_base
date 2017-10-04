9f67011afdad5210d4f2a3fe166221c472814242
Andreas Blaesius <skate4life@gmx.de>
2017-10-04 09:40:57 -0400

    Add Slim Framework resource APK to Zygote FD whitelist
    * Ported to O from https://review.slimroms.org/#/c/22766/
    
    Change-Id: I0344dac461447c245d243c022246c35a98d7987e

--- core/jni/fd_utils.cpp
+++ core/jni/fd_utils.cpp
@@ -39,6 +39,7 @@ static const char* kPathWhitelist[] = {
   "/dev/socket/webview_zygote",
   "/sys/kernel/debug/tracing/trace_marker",
   "/system/framework/framework-res.apk",
+  "/system/framework/org.slim.framework-res.apk",
   "/dev/urandom",
   "/dev/ion",
   "/dev/dri/renderD129", // Fixes b/31172436
