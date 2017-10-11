7d8152a6a88baf120dc596f07eb3822fab01bc90
Dan Cartier <nospamdan1@gmail.com>
2017-10-11 13:20:21 -0400

    [1/3] frameworks_base: Add ProximityWake
    Yank555.lu : remove config for default value, settings potentially won't
                 be set correctly on first boot anyway, default to stock
                 Android (disabled)
    
    ProximityWake : Add support for checking proximity when waking device
    
    Be sure to shutdown the proximity sensor after display wake
    
    This fixes:
    http://review.cyanogenmod.org/#/c/66657/
    
    PowerManager : Make proximity check opt-in
    
    Framework : Fix proximity wakeup reference
    
    [1/2] Frameworks: let InCallUI handle proximity sensor for incoming calls
    
    Fixes "while 'prevent accidental wake-up' is on, proximity sensor
    doesn't control screen state on incoming call. Must manually
    turn on screen to see who's calling."
    
    power: Fix locking for wakeup proximity check
    
     * Often I'm noticing high power consumption due to the proximity sensor
       never turning off. Add synchronization around this code to ensure
       ordering.
    
    add config for proximity check on screen on default value
    
    Services : Ensure proximity check is taken into consideration
    
    Only do the proximity check if explicitly being asked to do so.
    This is a patch that was never ported from cm-11.0 :
    2293e60c7de011782e59486605c04b9f67a1e24f
    
    issue-id: CYNGNOS-1223
    
    services: button brightness should check proximity
    
    base: Fix proximity check on power key (Christopher R. Palmer <crpalmer@gmail.com>)
    
    To test, enable "Settings >> Display >> Prevent accidental wakeup",
    hold your hand over the proximity sensor and turn your phone on.
    
    Prior to this change, it will wake up the phone and after the change
    it will not.
    
    powermanager: Initialize SensorManager in systemReady() (Steve Kondik <steve@cyngn.com>)
    
     * Service startup ordering is different on M and the sensor service
       is not ready during construction of the PowerManager. This was
       causing a hang on boot. Move it to systemReady instead.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/config.xml
    	core/res/res/values/yaosp_symbols.xml
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	services/core/java/com/android/server/power/PowerManagerService.java
    
    Set mProximity to PROXIMITY_UNKNOWN on screen off
    
    In the case where the screen was already off do to the proximity
    sensor and the user presses the power button, we need to set
    mProximity to PROXIMITY_UNKNOWN so that when the screen comes back
    on and the proximity sensor is re-enabled it will be able to pick
    up the change in proximity.
    
    Change-Id: I79bc948e4a5b64009b81879aba6b31279b7c5240
    Signed-off-by: Danesh M <daneshm90@gmail.com>
    Signed-off-by: Daniel Koman <dankoman30@gmail.com>
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6626,10 +6626,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void wakeUpFromPowerKey(long eventTime) {
-        wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey, "android.policy:POWER");
+        wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey, "android.policy:POWER", true);
     }
 
     private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, String reason) {
+        return wakeUp(wakeTime, wakeInTheaterMode, reason, false);
+    }
+
+    private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, String reason,
+            boolean withProximityCheck) {
         final boolean theaterModeEnabled = isTheaterModeEnabled();
         if (!wakeInTheaterMode && theaterModeEnabled) {
             return false;
@@ -6640,7 +6645,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.THEATER_MODE_ON, 0);
         }
 
-        mPowerManager.wakeUp(wakeTime, reason);
+        if (withProximityCheck) {
+            mPowerManager.wakeUpWithProximityCheck(wakeTime, reason);
+        } else {
+            mPowerManager.wakeUp(wakeTime, reason);
+        }
         return true;
     }
 
